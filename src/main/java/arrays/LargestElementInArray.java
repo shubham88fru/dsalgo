7332041e6e2cd2558392ae64962dde3cd6030c18
shubham88fru <shubham88fru@yahoo.com>
2022-07-02 12:52:43 +0530

    find largest element in array

---
+++ src/main/java/arrays/LargestElementInArray.java
@@ -0,0 +1,36 @@
+package arrays;
+
+public class LargestElementInArray {
+    //Naive approach
+    //T: O(N^2)
+    static int getLargestElIndex(int arr[]) {
+        int n = arr.length;
+        for (int i = 0; i<n; i++) {
+            boolean flag = true;
+            for (int j = 0; j<n; j++) {
+                if (arr[j] > arr[i]) {
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag) return i;
+        }
+        return -1;
+    }
+
+    //T: O(N)
+    static int getLargestElIndex2(int arr[]) {
+        int n = arr.length;
+        int largestIdx = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] >= arr[largestIdx]) largestIdx = i;
+        }
+        return largestIdx;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 21, 2 ,300, 42, 5};
+        //System.out.println(getLargestElIndex(arr));
+        System.out.println(getLargestElIndex2(arr));
+    }
+}

6cf8db21ccac03371fba90c786a4a5b0a99dce65
shubham88fru <shubham88fru@yahoo.com>
2022-07-09 11:31:17 +0530

    print 1 to n tail recursive

--- src/main/java/recursion/Print1ToN.java
+++ src/main/java/recursion/Print1ToN.java
@@ -9,7 +9,15 @@ public class Print1ToN {
         System.out.print(n+" ");
     }
 
+    static void print1ToNTailRecursive(int n, int k) {
+        if (n==0) return;
+        System.out.print(k+" ");
+        print1ToNTailRecursive(n-1, k+1);
+    }
+
     public static void main(String[] args) {
         print1ToN(9);
+        System.out.println();
+        print1ToNTailRecursive(9, 1); //k must be called with 1
     }
 }

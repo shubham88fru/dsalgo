e6b0cacbb5518edd4faf706cc58a5aa28f7e1716
shubham88fru <shubham88fru@yahoo.com>
2022-07-09 12:45:53 +0530

    fibonacci using recursion

---
+++ src/main/java/recursion/Fibonacci.java
@@ -0,0 +1,20 @@
+package recursion;
+
+//Find value of fibonacci series
+//at nth index.
+//n>=0
+public class Fibonacci {
+
+    static int fib(int n) {
+        // if (n<=1) return n;
+        if (n==0) return 0;
+        if (n==1) return 1;
+
+        //0 1 1 2 3 5 8 13
+        return fib(n-1) + fib(n-2);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(fib(7));
+    }
+}

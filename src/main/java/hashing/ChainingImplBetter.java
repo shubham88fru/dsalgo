fca5fe84d0794e270e2b679e565cef05ecbea80c
shubham88fru <shubham88fru@yahoo.com>
2022-07-13 12:08:42 +0530

    implementing simple hashing collision avoidance strategy - chaining

---
+++ src/main/java/hashing/ChainingImplBetter.java
@@ -0,0 +1,54 @@
+package hashing;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class ChainingImplBetter {
+    private int mod;
+    private ArrayList<LinkedList<Integer>> hashTable;
+
+    ChainingImplBetter(int modSize) {
+        mod = modSize;
+        hashTable = new ArrayList<LinkedList<Integer>>();
+        for (int i=0; i<mod; i++) {
+            hashTable.add(new LinkedList<>());
+        }
+    }
+
+    void insert(int key) {
+        int idx = getIdx(key);
+        hashTable.get(idx).add(key);
+    }
+
+    boolean search(int key) {
+        int idx = getIdx(key);
+        return hashTable.get(idx).contains(key);
+    }
+
+    int delete(int key) {
+        int idx = getIdx(key);
+        if (hashTable.get(idx)!=null) {
+            if (hashTable.get(idx).contains(key))
+                hashTable.get(idx).remove(Integer.valueOf(key));
+            return idx;
+        }
+        return -1;
+    }
+
+    private int getIdx(int key) {
+        return key%mod;
+    }
+
+    public static void main(String[] args) {
+        ChainingImplBetter chaining = new ChainingImplBetter(7);
+        chaining.insert(70);
+        chaining.insert(71);
+        chaining.insert(9);
+        chaining.insert(56);
+        chaining.insert(72);
+        chaining.delete(56);
+
+        System.out.println(chaining.search(56));
+        System.out.println(chaining.search(51));
+    }
+}
---
+++ src/main/java/hashing/ChainingImplNaive.java
@@ -0,0 +1,57 @@
+package hashing;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+
+public class ChainingImplNaive {
+    private static LinkedList<Integer>[] arr;
+    private static int mod;
+
+    public ChainingImplNaive(int size) {
+        mod = size;
+        // This is bad, java doesn't
+        // allow arrays of  generics.
+        arr = new LinkedList[size];
+        Arrays.fill(arr, null);
+    }
+
+    static void insert(int key) {
+        int idx = getIdx(key);
+        if (arr[idx]==null) {
+            arr[idx] = new LinkedList<>();
+        }
+        arr[idx].add(key);
+    }
+
+    static boolean search(int key) {
+        int idx = getIdx(key);
+        return arr[idx].contains(key);
+    }
+
+    static int delete(int key) {
+        int idx = getIdx(key);
+        if (arr[idx] != null) {
+            if (arr[idx].contains(key))
+                arr[idx].remove(Integer.valueOf(key));
+            return idx;
+        }
+        return -1;
+    }
+
+    static int getIdx(int key) {
+        return key%mod;
+    }
+
+    public static void main(String[] args) {
+        ChainingImplNaive chaining = new ChainingImplNaive(7);
+        chaining.insert(70);
+        chaining.insert(71);
+        chaining.insert(9);
+        chaining.insert(56);
+        chaining.insert(72);
+        chaining.delete(56);
+
+        System.out.println(chaining.search(56));
+        System.out.println(chaining.search(51));
+    }
+}

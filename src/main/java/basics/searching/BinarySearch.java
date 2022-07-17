063ffc073b782b2e522c52ef035d9f34a0185c8e
Shubham Singh <shubham88fru@yahoo.com>
2022-07-17 11:25:14 +0530

    recursive binary search

--- src/main/java/basics/searching/BinarySearch.java
+++ src/main/java/basics/searching/BinarySearch.java
@@ -7,20 +7,35 @@ import java.util.Arrays;
 public class BinarySearch {
 
     //Doesn't work
-    //TODO: Check binary search implementation using recursion.
-    int binarySearchMy(int[] arr, int el) {
+    int binarySearchMyRecursive(int[] arr, int el) {
         int mid = (arr.length/2);
         if (mid<0) return -1;
         if (arr[mid] == el) {
             return mid;
         } else if (el < arr[mid]) {
-            return binarySearchMy(Arrays.copyOfRange(arr, 0, mid), el);
+            return binarySearchMyRecursive(Arrays.copyOfRange(arr, 0, mid), el);
         } else {
-            return binarySearchMy(Arrays.copyOfRange(arr, mid, arr.length), el);
+            return binarySearchMyRecursive(Arrays.copyOfRange(arr, mid, arr.length), el);
         }
     }
 
-    int binarySearchSir(int[] arr, int el) {
+    //T:O(log(n)), S:O(log(n))
+    int binarySearchSirRecursive(int[] arr, int low, int high, int el) {
+        int mid = (low+high)/2;
+        if (low>high) return -1;
+        if (arr[mid] == el) {
+            return mid;
+        } else if (el < arr[mid]) {
+            high = mid - 1;
+            return binarySearchSirRecursive(arr, low, high, el);
+        } else {
+            low = mid + 1;
+            return binarySearchSirRecursive(arr, low, high, el);
+        }
+    }
+
+    //T:O(log(n)), S:O(1)
+    int binarySearchSirIterative(int[] arr, int el) {
         int start = 0, mid;
         int last = arr.length - 1;
         while (start<=last) {
@@ -37,8 +52,14 @@ public class BinarySearch {
 
     public static void main(String[] args) {
         BinarySearch bs = new BinarySearch();
-        System.out.println(bs.binarySearchMy(new int[] {10, 20, 30, 40, 50, 60}, 50));
-        System.out.println(bs.binarySearchSir(new int[] {10, 20, 30, 40, 50, 60}, 60));
-        System.out.println(bs.binarySearchSir(new int[] {10, 20, 30}, 25));
+        System.out.println(bs.binarySearchMyRecursive(new int[] {10, 20, 30, 40, 50, 60}, 50));
+        System.out.println("-------------------------");
+
+        System.out.println(bs.binarySearchSirIterative(new int[] {10, 20, 30, 40, 50, 60}, 60));
+        System.out.println(bs.binarySearchSirIterative(new int[] {10, 20, 30}, 25));
+        System.out.println("-------------------------");
+
+
+        System.out.println(bs.binarySearchSirRecursive(new int[] {10, 20, 30}, 0, 2, 30));
     }
 }

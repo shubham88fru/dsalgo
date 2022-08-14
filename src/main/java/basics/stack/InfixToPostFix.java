26ed35565042a1a90f8dc8b24c04803ef6159f16
shubham88fru <shubham88fru@yahoo.com>
2022-08-14 12:02:05 +0530

    incomplete infix to postfix

---
+++ src/main/java/basics/stack/InfixToPostFix.java
@@ -0,0 +1,25 @@
+package basics.stack;
+
+/*
+Considering limited set of operators -
+^, * , /, +, -
+And below associativity rules (in decreasing order
+of precedence)
+--------------------------
+Operators | Associativity |
+--------------------------
+^         | Right to Left |
+* /       | Left to Right |
++ -       | Left to Right |
+--------------------------
+*/
+public class InfixToPostFix {
+
+    String getPostFix(String infixStr) {
+        return null;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}

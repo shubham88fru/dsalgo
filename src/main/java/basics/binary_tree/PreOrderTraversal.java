c3225134cc742263130663fe63ee08b0da96a53d
shubham88fru <shubham88fru@yahoo.com>
2022-08-24 09:34:43 +0530

    preorder traversal

---
+++ src/main/java/basics/binary_tree/PreOrderTraversal.java
@@ -0,0 +1,30 @@
+package basics.binary_tree;
+
+//Given root of a binary tree, print
+//the inorder traversal of the tree.
+public class PreOrderTraversal {
+
+    void preorder(Node root) {
+        if (root != null) {
+            //root - left - right
+            System.out.print(root.key+" ");
+            preorder(root.left);
+            preorder(root.right);
+        }
+    }
+
+    public static void main(String[] args) {
+        Node root = new Node(10);
+        root.left = new Node(20);
+        root.right = new Node(30);
+        root.right.left = new Node(40);
+        root.right.right = new Node(50);
+
+        PreOrderTraversal preOrderTraversal =
+                new PreOrderTraversal();
+        preOrderTraversal.preorder(root);
+        System.out.println();
+        System.out.println("-------------");
+        preOrderTraversal.preorder(root);
+    }
+}

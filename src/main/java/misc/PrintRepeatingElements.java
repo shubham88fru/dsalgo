5df8ef4ca4411379f34d993bf54a4b2080ce7558
shubham88fru <shubham88fru@yahoo.com>
2022-09-04 10:00:49 +0530

    print repeating els

---
+++ src/main/java/misc/PrintRepeatingElements.java
@@ -0,0 +1,24 @@
+package misc;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class PrintRepeatingElements {
+
+    void printRepeating(int[] arr) {
+        Set<Integer> set =
+                new HashSet<>();
+        for (int j : arr) {
+            if (set.contains(j))
+                System.out.println(j + " ");
+            else set.add(j);
+        }
+    }
+
+    public static void main(String[] args) {
+        PrintRepeatingElements printRepeatingElements
+                = new PrintRepeatingElements();
+        printRepeatingElements
+                .printRepeating(new int[]{10, 8, 10, 8});
+    }
+}

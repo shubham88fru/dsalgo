79ea222754774b905bcccb95487fd0c5d3fd950e
shubham88fru <shubham88fru@yahoo.com>
2022-09-08 11:07:55 +0530

    print frequencies in order

--- src/main/java/misc/FrequencyOfElementsInArray.java
+++ src/main/java/misc/FrequencyOfElementsInArray.java
@@ -1,10 +1,12 @@
 package misc;
 
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class FrequencyOfElementsInArray {
 
+    //dont care about order
     void printFrequency(int[] arr) {
         Map<Integer, Integer> fmap = new HashMap<>();
         for (int a: arr) {
@@ -12,6 +14,19 @@ public class FrequencyOfElementsInArray {
                     fmap.getOrDefault(a, 0)+1);
         }
         System.out.println(fmap);
+    }
+
+    //print in order of appearance in array.
+    void printFrequencyInOrder(int[] arr) {
+        Map<Integer, Integer> linkedFmap
+                = new LinkedHashMap<>();
+        for (int a: arr)
+            linkedFmap.put(
+                    a,
+                    linkedFmap
+                       .getOrDefault(a, 0)+1
+            );
+        System.out.println(linkedFmap);
     }
 
     public static void main(String[] args) {
@@ -22,5 +37,8 @@ public class FrequencyOfElementsInArray {
         int[] arr = {10, 5, 20, 5, 10, 5};
         frequencyOfElementsInArray
                 .printFrequency(arr);
+
+        frequencyOfElementsInArray
+                .printFrequencyInOrder(arr);
     }
 }

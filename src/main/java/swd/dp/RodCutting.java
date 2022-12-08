dd59c0bcb929f9058146a1b3a9a63ce486a6dbad
Shubham Singh <shubham88fru@yahoo.com>
2022-12-08 11:15:32 +0530

    rod cutting problem

---
+++ src/main/java/swd/dp/RodCutting.java
@@ -0,0 +1,33 @@
+package swd.dp;
+
+import java.util.HashMap;
+import java.util.Map;
+
+//@link - https://practice.geeksforgeeks.org/problems/rod-cutting0840/1?utm_source=gfg&utm_medium=article&utm_campaign=bottom_sticky_on_article
+public class RodCutting {
+
+    public int cutRod(int price[], int n) {
+        return cutRodForMaxPrice(price, n, 0, new HashMap<String, Integer>());
+    }
+
+    private int cutRodForMaxPrice(int[] price, int remainingLength, int currIndex, Map<String, Integer> memo) {
+
+        if (currIndex >= price.length) return -1000001;
+        if (remainingLength == 0) return 0;
+
+        int includeCurr = -1000001;
+
+        String key = currIndex + "-" + remainingLength;
+        if (memo.containsKey(key)) return memo.get(key);
+
+        if ((currIndex+1) <= remainingLength) {
+            includeCurr = price[currIndex] +
+                    cutRodForMaxPrice(price, remainingLength-(currIndex+1), currIndex, memo);
+        }
+
+        int dontIncludeCurr = cutRodForMaxPrice(price, remainingLength, currIndex+1, memo);
+
+        memo.put(key, Math.max(includeCurr, dontIncludeCurr));
+        return memo.get(key);
+    }
+}

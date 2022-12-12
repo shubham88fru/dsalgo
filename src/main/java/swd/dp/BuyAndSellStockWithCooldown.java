6e5fc52ced905a35a8017688bcbd2123531163fa
Shubham Singh <shubham88fru@yahoo.com>
2022-12-12 11:22:25 +0530

    buy and sell stock with cooldown

---
+++ src/main/java/swd/dp/BuyAndSellStockWithCooldown.java
@@ -0,0 +1,33 @@
+package swd.dp;
+
+import java.util.HashMap;
+import java.util.Map;
+
+//@link - https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-cooldown/description/
+public class BuyAndSellStockWithCooldown {
+    //Can do any no. of transactions
+    /** SWD SOLN **/
+    public int maxProfit(int[] prices) {
+        return bestTimeToBuyAndSellForMaxProfit(prices, 0, 1, new HashMap<String, Integer>());
+    }
+
+    private int bestTimeToBuyAndSellForMaxProfit(int[] prices, int currentDay, int canBuy, Map<String, Integer> memo) {
+        if (currentDay>=prices.length) return 0;
+
+        String key = currentDay + "-" + canBuy;
+
+        if (memo.containsKey(key)) return memo.get(key);
+
+        int idle = bestTimeToBuyAndSellForMaxProfit(prices, currentDay+1, canBuy, memo);
+        if (canBuy == 1) {
+            int buy = -prices[currentDay]
+                    + bestTimeToBuyAndSellForMaxProfit(prices, currentDay+1, 0, memo);
+            memo.put(key, Math.max(idle, buy));
+        } else {
+            int sell = prices[currentDay]
+                    + bestTimeToBuyAndSellForMaxProfit(prices, currentDay+2, 1, memo);
+            memo.put(key, Math.max(idle, sell));
+        }
+        return memo.get(key);
+    }
+}

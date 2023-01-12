e514787bfa86dcbecdacb34e7dd73a4a7bce04b6
Shubham Singh <shubham88fru@yahoo.com>
2023-01-12 10:47:05 +0530

    sub arrays with equal 0s 1s and 2s

---
+++ src/main/java/swd/hashmap/SubArraysWithEqual0s1sAnd2s.java
@@ -0,0 +1,32 @@
+package swd.hashmap;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SubArraysWithEqual0s1sAnd2s {
+    long getSubstringWithEqual012(String s) {
+        // code here
+        long ans = 0;
+
+        int z0 = 0;
+        int z1 = 0;
+        int z2 = 0;
+
+        Map<String, Integer> memo = new HashMap<>();
+        memo.put((z1-z0) + "#" + (z2-z1), 1); //putting (0#0, 1) initially.
+
+        for (int i=0; i<s.length(); i++) {
+            if (s.charAt(i) == '0') z0 += 1;
+            else if (s.charAt(i) == '1') z1 += 1;
+            else z2 += 1;
+
+            String exp = (z1-z0) + "#" + (z2-z1);
+
+            if (memo.containsKey(exp)) {
+                ans += memo.get(exp);
+                memo.put(exp, memo.get(exp)+1);
+            } else memo.put(exp, 1);
+        }
+        return ans;
+    }
+}

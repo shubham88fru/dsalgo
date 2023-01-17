c6abe760ff8c5c3d9c6b1248bd7c7f81bec32b40
Shubham Singh <shubham88fru@yahoo.com>
2023-01-17 12:05:20 +0530

    rabbits in forest

---
+++ src/main/java/swd/hashmap/RabbitsInForest.java
@@ -0,0 +1,30 @@
+package swd.hashmap;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+//@link - https://leetcode.com/problems/rabbits-in-forest/description/
+public class RabbitsInForest {
+    /** My Soln - Works fine **/
+    public int numRabbits(int[] answers) {
+        Map<Integer, Integer> groups = new HashMap<>();
+        int minRabits = 0;
+        for (int answer : answers) {
+            //Create a new group if answer not seen before
+            //or if seen before but space exhausted
+            //of if no other rabbit of same color
+            if (!groups.containsKey(answer) || Objects.equals(groups.get(answer), 0) || answer == 0) {
+                //We need to find the min possible no. of rabbits
+                //so we'll assume that the rabbits that have not reported
+                //have not seen any extra rabbit of same color.
+                groups.put(answer, answer);
+                minRabits += answer + 1;
+            } else {
+                //else keep using up the space.
+                groups.put(answer, groups.get(answer) - 1);
+            }
+        }
+        return minRabits;
+    }
+}

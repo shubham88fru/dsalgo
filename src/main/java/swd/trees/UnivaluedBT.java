39e5be1c5a50f2f02292a38c0d3c982cbad5f1f0
Shubham Singh <shubham88fru@yahoo.com>
2023-01-26 19:18:16 +0530

    univalued bt

---
+++ src/main/java/swd/trees/UnivaluedBT.java
@@ -0,0 +1,17 @@
+package swd.trees;
+
+//@link - https://leetcode.com/problems/univalued-binary-tree/description/
+public class UnivaluedBT {
+    public boolean isUnivalTree(TreeNode root) {
+        return isUnivalued(root, root.val);
+    }
+
+    private boolean isUnivalued(TreeNode root, int val) {
+        //If reached till leaf and we couldn't find a node which is unmatching, return true
+        if (root == null) return true;
+
+        //traverse preorder and check if
+        //curr nodes val is same as root's val and left subtree and right subtree are univalued.
+        return (val == root.val) && isUnivalued(root.left, val) && isUnivalued(root.right, val);
+    }
+}

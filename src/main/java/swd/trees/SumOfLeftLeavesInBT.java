6bb656438be4a609b73c294fd68869d524114a17
shubham88fru <shubham88fru@yahoo.com>
2023-01-28 11:34:57 +0530

    sum of left leaves in bt

---
+++ src/main/java/swd/trees/SumOfLeftLeavesInBT.java
@@ -0,0 +1,36 @@
+package swd.trees;
+
+public class SumOfLeftLeavesInBT {
+    /*** My Soln - Works but kinda nasty code. ***/
+    public int sumOfLeftLeaves(TreeNode root) {
+        return dfsForLeftLeaves(root, false, 0);
+    }
+
+    private int dfsForLeftLeaves(TreeNode root, boolean isLeftChild, int sum) {
+        if (root == null) return sum;
+        if (root.left == null && root.right == null && isLeftChild) {
+            sum += root.val;
+        }
+
+        sum = dfsForLeftLeaves(root.left, true, sum);
+        sum = dfsForLeftLeaves(root.right, false, sum);
+        return sum;
+    }
+
+    /*** SWD Soln ***/
+    public int sumOfLeftLeaves2(TreeNode root) {
+        return dfsForLeftLeaves2(root, false);
+    }
+
+    private int dfsForLeftLeaves2(TreeNode root, boolean isLeftChild) {
+        if (root == null) return 0;
+        if (root.left == null && root.right == null) {
+            return isLeftChild ? root.val : 0;
+        }
+
+        int leftAns = dfsForLeftLeaves2(root.left, true);
+        int rightAns = dfsForLeftLeaves2(root.right, false);
+
+        return leftAns + rightAns;
+    }
+}

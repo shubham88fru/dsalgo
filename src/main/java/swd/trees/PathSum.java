6e9390356a373ea41d19620bd593341d02e9feb6
Shubham Singh <shubham88fru@yahoo.com>
2023-01-28 19:55:15 +0530

    path sum

---
+++ src/main/java/swd/trees/PathSum.java
@@ -0,0 +1,26 @@
+package swd.trees;
+
+//@link - https://leetcode.com/problems/path-sum/description/
+public class PathSum {
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        return dfsForSum(root, targetSum);
+    }
+
+    //Pre-order traverse for target sum.
+    private boolean dfsForSum(TreeNode root, int targetSum) {
+        if (root == null) return false;
+
+        //if leaf node check current targetsum equals
+        //node value.
+        if (root.left == null && root.right == null) {
+            if (targetSum == root.val) return true;
+        }
+
+        //recursively traverse pre-order for target sum.
+        targetSum -= root.val;
+        boolean leftAns = dfsForSum(root.left, targetSum);
+        boolean rightAns = dfsForSum(root.right, targetSum);
+
+        return (leftAns || rightAns);
+    }
+}

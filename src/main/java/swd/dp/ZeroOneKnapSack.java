9d79aa3e5c91a447076cac81ef6ee6d5290a17aa
shubham88fru <shubham88fru@yahoo.com>
2023-01-30 10:06:38 +0530

    add link

--- src/main/java/swd/dp/ZeroOneKnapSack.java
+++ src/main/java/swd/dp/ZeroOneKnapSack.java
@@ -3,9 +3,10 @@ package swd.dp;
 import java.util.HashMap;
 import java.util.Map;
 
+//@link - https://practice.geeksforgeeks.org/problems/0-1-knapsack-problem0945/1?utm_source=gfg&utm_medium=article&utm_campaign=bottom_sticky_on_article
+
 public class ZeroOneKnapSack {
 
-    //@link - https://practice.geeksforgeeks.org/problems/0-1-knapsack-problem0945/1?utm_source=gfg&utm_medium=article&utm_campaign=bottom_sticky_on_article
     static int knapSack(int W, int wt[], int val[], int n)
     {
         return maxProfitForWeightLimit(W, wt, val, 0, new HashMap<String, Integer>());

7730d8777fbbec6c8ed98b79ef0178e9f7263977
Shubham Singh <shubham88fru@yahoo.com>
2023-02-01 11:36:31 +0530

    post order traversal of n-ary tree

--- src/main/java/swd/trees/PostorderTraversalOfNAT.java
+++ src/main/java/swd/trees/PostorderTraversalOfNAT.java
@@ -1,17 +1,17 @@
 package swd.trees;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 //@link - https://leetcode.com/problems/n-ary-tree-postorder-traversal/description/
 public class PostorderTraversalOfNAT {
     public List<Integer> postorder(Node root) {
         List<Integer> ans = new ArrayList<>();
-        traversePostOrder(root, ans);
+        traversePostOrderIterative(root, ans);
+        // traversePostOrder(root, ans);
         return ans;
     }
 
-    //Recursive algorithm for post order traversal of n-ary tree.
+    //1) Recursive algorithm for post order traversal of n-ary tree.
     private void traversePostOrder(Node root, List<Integer> ans) {
         if (root == null) return;
 
@@ -22,5 +22,25 @@ public class PostorderTraversalOfNAT {
 
         //visit root.
         ans.add(root.val);
+    }
+
+    //2) Iterative algorithm
+    private void traversePostOrderIterative(Node root, List<Integer> ans) {
+        if (root == null) return;
+        Deque<Node> stack = new ArrayDeque<>();
+        stack.addFirst(root);
+
+        while (!stack.isEmpty()) {
+            Node currNode = stack.removeFirst();
+
+            //Trick: process in reverse order of an actual postorder traversal
+            //i.e. Node - Right - Left
+            ans.add(currNode.val);
+            for (Node child: currNode.children) {
+                stack.addFirst(child);
+            }
+        }
+        //Finally reverse the list.
+        Collections.reverse(ans);
     }
 }

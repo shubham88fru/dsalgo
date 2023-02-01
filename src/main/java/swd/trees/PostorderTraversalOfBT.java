4495bfc9c3a1d4c53778142570cdd00dd3024c94
Shubham Singh <shubham88fru@yahoo.com>
2023-02-01 11:20:02 +0530

    post order traversal iterative soln

--- src/main/java/swd/trees/PostorderTraversalOfBT.java
+++ src/main/java/swd/trees/PostorderTraversalOfBT.java
@@ -1,21 +1,41 @@
 package swd.trees;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 //@link - https://leetcode.com/problems/binary-tree-postorder-traversal/
 public class PostorderTraversalOfBT {
     public List<Integer> postorderTraversal(TreeNode root) {
         List<Integer> ans = new ArrayList<>();
-        traversePostOrder(root, ans);
+        traversePostOrderIterative(root, ans);
+        // traversePostOrder(root, ans);
         return ans;
     }
-
+    //1) Recursive algorithm
     private void traversePostOrder(TreeNode root, List<Integer> ans) {
         if (root == null) return;
         traversePostOrder(root.left, ans);
         traversePostOrder(root.right, ans);
         ans.add(root.val);
+    }
 
+
+    //2) Iterative algorithm
+    private void traversePostOrderIterative(TreeNode root, List<Integer> ans) {
+        if (root == null) return;
+        Deque<TreeNode> stack = new ArrayDeque<>();
+
+        stack.addFirst(root);
+
+        //Trick: process in reverse order of an actual postorder traversal
+        //i.e. Node - Right - Left
+        while (!stack.isEmpty()) {
+            TreeNode currNode = stack.removeFirst();
+            ans.add(currNode.val);
+            if (currNode.left != null) stack.addFirst(currNode.left);
+            if (currNode.right != null) stack.addFirst(currNode.right);
+        }
+
+        //Finally reverse the list.
+        Collections.reverse(ans);
     }
 }

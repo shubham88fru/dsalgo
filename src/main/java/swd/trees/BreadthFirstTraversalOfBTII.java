9038e81efd13987c7f8e6ad45205b1bf1258ac39
Shubham Singh <shubham88fru@yahoo.com>
2023-02-10 09:20:16 +0530

    bfs of bt 2

---
+++ src/main/java/swd/trees/BreadthFirstTraversalOfBTII.java
@@ -0,0 +1,54 @@
+package swd.trees;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+
+//@link - https://leetcode.com/problems/binary-tree-level-order-traversal/description/
+public class BreadthFirstTraversalOfBTII {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> bfsTraversal = new ArrayList<>();
+
+        bfs(root, bfsTraversal);
+
+        return bfsTraversal;
+    }
+
+    private void bfs(TreeNode root, List<List<Integer>> bfsTraversal) {
+        if (root == null) return;
+
+        //Initialize the queue. We use a queue for BF Traversal.
+        Deque<TreeNode> queue = new ArrayDeque<>();
+
+        queue.addLast(root);
+
+        while (!queue.isEmpty()) {
+            List<Integer> level = new ArrayList<>();
+
+            //At any level, queue will have only the nodes at
+            //that level.
+            int remainingNodeToProcess = queue.size();
+
+            //Process all nodes at current level
+            while (remainingNodeToProcess != 0) {
+                //keep removing the nodes untill
+                //all nodes of that level are processed.
+                TreeNode currNode = queue.removeFirst();
+                level.add(currNode.val);
+                remainingNodeToProcess -= 1;
+
+                //Add left child of curr node to queue.
+                if (currNode.left != null) {
+                    queue.addLast(currNode.left);
+                }
+
+                //Add right child of curr node to queue.
+                if (currNode.right != null) {
+                    queue.addLast(currNode.right);
+                }
+            }
+            bfsTraversal.add(level);
+        }
+    }
+}

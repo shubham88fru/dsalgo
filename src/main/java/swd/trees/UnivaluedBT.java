2f851df3f422d6a27a01d2ceb9e32ac84c0ee5e4
Shubham Singh <shubham88fru@yahoo.com>
2023-02-11 19:23:35 +0530

    univalued bt bfs

--- src/main/java/swd/trees/UnivaluedBT.java
+++ src/main/java/swd/trees/UnivaluedBT.java
@@ -1,17 +1,60 @@
 package swd.trees;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 //@link - https://leetcode.com/problems/univalued-binary-tree/description/
 public class UnivaluedBT {
     public boolean isUnivalTree(TreeNode root) {
-        return isUnivalued(root, root.val);
+        //return isUnivaluedDFS(root, root.val);
+        return isUnivaluedBFS(root);
     }
 
-    private boolean isUnivalued(TreeNode root, int val) {
-        //If reached till leaf and we couldn't find a node which is un-matching, return true
+    //1) DFS algorithm for this problem.
+    private boolean isUnivaluedDFS(TreeNode root, int val) {
+        //If reached till leaf and we couldn't find a node which is unmatching, return true
         if (root == null) return true;
 
         //traverse preorder and check if
         //curr nodes val is same as root's val and left subtree and right subtree are univalued.
-        return (val == root.val) && isUnivalued(root.left, val) && isUnivalued(root.right, val);
+        return (val == root.val) && isUnivaluedDFS(root.left, val) && isUnivaluedDFS(root.right, val);
+    }
+
+    //2) BFS algorithm for this problem.
+    private boolean isUnivaluedBFS(TreeNode root) {
+        if (root == null) return true;
+
+        //Initialize queue for bfs.
+        Deque<TreeNode> queue = new ArrayDeque<>();
+        queue.addLast(root);
+
+        //value to be matched with every node.
+        int target = root.val;
+
+        //run bfs.
+        while (!queue.isEmpty()) {
+            //At any level, no. of nodes in queue will
+            //be equal to nodes at that level.
+            int nodesToProcessAtCurrLevel = queue.size();
+
+
+            //Process all nodes at the curr level.
+            while (nodesToProcessAtCurrLevel > 0) {
+                TreeNode currNode = queue.removeFirst();
+
+                //if any node's value is diff, break immediately
+                //and return false.
+                if (currNode.val != target) return false;
+
+                if (currNode.left != null) queue.addLast(currNode.left);
+                if (currNode.right != null) queue.addLast(currNode.right);
+
+                nodesToProcessAtCurrLevel -= 1;
+            }
+        }
+
+        //If here, means all nodes value are same.
+        //Hence, tree must be univalued.
+        return true;
     }
 }

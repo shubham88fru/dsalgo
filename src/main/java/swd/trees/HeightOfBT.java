12f25386f41c3ed43c7c823b4508717404f32c8c
Shubham Singh <shubham88fru@yahoo.com>
2023-02-11 13:03:31 +0530

    bfs

--- src/main/java/swd/trees/HeightOfBT.java
+++ src/main/java/swd/trees/HeightOfBT.java
@@ -1,25 +1,61 @@
 package swd.trees;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 //@link - https://leetcode.com/problems/maximum-depth-of-binary-tree/description/
 public class HeightOfBT {
     public int maxDepth(TreeNode root) {
         //Max depth of BT is nothing but its height.
-        return heightOfBT(root);
+
+        //return heightOfBTDFS(root);
+        return heightOfBTBFS(root);
     }
 
-    //Recursive algorithm to find height of BT.
-    private int heightOfBT(TreeNode root) {
+    //1) Recursive (DFS) algorithm to find height of BT.
+    private int heightOfBTDFS(TreeNode root) {
         //Base case. If already on a leaf node, no height.
         if (root == null) return 0;
 
         //height of left sub tree from a node.
-        int left = 1 + heightOfBT(root.left);
+        int left = 1 + heightOfBTDFS(root.left);
 
         //height of right sub tree from a node.
-        int right = 1 + heightOfBT(root.right);
+        int right = 1 + heightOfBTDFS(root.right);
 
         //find the max.
         return Math.max(left, right);
+    }
+
+    //2) Level order traversal approach
+    private int heightOfBTBFS(TreeNode root) {
+        if (root == null) return 0;
+
+        //Initialize queue for bfs.
+        Deque<TreeNode> queue = new ArrayDeque<>();
+        queue.addLast(root);
+
+        int level = 0;
+
+        while (!queue.isEmpty()) {
+            //At any level, num of nodes in queue is equal
+            //to num of nodes at the level.
+            int nodesOnLevel = queue.size();
+
+            //For each node at curr level, add their children
+            //to the queue.
+            while (nodesOnLevel > 0) {
+                TreeNode currNode = queue.removeFirst();
+                if (currNode.left != null) queue.addLast(currNode.left);
+                if (currNode.right != null) queue.addLast(currNode.right);
+                nodesOnLevel -= 1;
+            }
+
+            //increment level at each level.
+            level += 1;
+        }
+
+        return level;
     }
 }
 

70fe3e3549eb20210995f4f0823e91314bc0fc64
Shubham Singh <shubham88fru@yahoo.com>
2023-02-13 11:08:25 +0530

    sum of left leaves bfs

--- src/main/java/swd/trees/SumOfLeftLeavesInBT.java
+++ src/main/java/swd/trees/SumOfLeftLeavesInBT.java
@@ -1,5 +1,8 @@
 package swd.trees;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 //@link - https://leetcode.com/problems/sum-of-left-leaves/description/
 public class SumOfLeftLeavesInBT {
     /*** My Soln - Works but kinda nasty code. ***/
@@ -7,6 +10,7 @@ public class SumOfLeftLeavesInBT {
         return dfsForLeftLeaves(root, false, 0);
     }
 
+    //1) DFS Algorithm.
     private int dfsForLeftLeaves(TreeNode root, boolean isLeftChild, int sum) {
         if (root == null) return sum;
         if (root.left == null && root.right == null && isLeftChild) {
@@ -23,6 +27,7 @@ public class SumOfLeftLeavesInBT {
         return dfsForLeftLeaves2(root, false);
     }
 
+    //1) DFS Algorithm
     private int dfsForLeftLeaves2(TreeNode root, boolean isLeftChild) {
         if (root == null) return 0;
         if (root.left == null && root.right == null) {
@@ -33,5 +38,39 @@ public class SumOfLeftLeavesInBT {
         int rightAns = dfsForLeftLeaves2(root.right, false);
 
         return leftAns + rightAns;
+    }
+
+    //2) BFS algorithm.
+    private int bfsForLeftLeaves(TreeNode root) {
+        if (root == null) return 0;
+
+        Deque<EnhancedNode> queue = new ArrayDeque<>();
+        queue.addLast(new EnhancedNode(root, false));
+
+        int ans = 0;
+        while (!queue.isEmpty()) {
+            EnhancedNode ennode = queue.removeFirst();
+            TreeNode currNode = ennode.node;
+            //if leaf and left child, add to sum.
+            if (currNode.left == null && currNode.right == null && ennode.isLeftChild) {
+                ans += currNode.val;
+            }
+
+            if (currNode.left != null) queue.addLast(new EnhancedNode(currNode.left, true));
+            if (currNode.right != null) queue.addLast(new EnhancedNode(currNode.right, false));
+        }
+        return ans;
+    }
+}
+
+//Wraps the TreeNode with the extra info
+//whether it is a left child or not.
+class EnhancedNode {
+    public TreeNode node;
+    public boolean isLeftChild;
+
+    public EnhancedNode(TreeNode node, boolean isLeftChild) {
+        this.node = node;
+        this.isLeftChild = isLeftChild;
     }
 }

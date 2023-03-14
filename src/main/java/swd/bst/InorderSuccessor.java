4305a1423b784cb8b7bcda6e5ae13b8e1ab9cd3a
Shubham Singh <shubham88fru@yahoo.com>
2023-03-14 11:53:05 +0530

    inorder successor

---
+++ src/main/java/swd/bst/InorderSuccessor.java
@@ -0,0 +1,38 @@
+package swd.bst;
+
+//@link - https://practice.geeksforgeeks.org/problems/inorder-successor-in-bst/1?utm_source=gfg&utm_medium=article&utm_campaign=bottom_sticky_on_article
+public class InorderSuccessor {
+    public GfgNode inorderSuccessor(GfgNode root, GfgNode x) {
+        boolean[] found = {false};
+        return inorder(root, x, found);
+    }
+
+    private GfgNode inorder(GfgNode root, GfgNode x, boolean[] found) {
+        if (root == null) return null;
+
+        GfgNode leftAns = inorder(root.left, x, found);
+        if (leftAns != null) return leftAns;
+
+        if (found[0]) {
+            return root;
+        }
+
+        if (root.data == x.data) {
+            found[0] = true;
+        }
+
+        GfgNode rightAns = inorder(root.right, x, found);
+        return rightAns;
+    }
+}
+
+
+class GfgNode {
+    int data;
+    GfgNode left, right;
+
+    GfgNode(int item) {
+        data = item;
+        left = right = null;
+    }
+}

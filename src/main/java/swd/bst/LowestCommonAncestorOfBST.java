500d8be2e961aace0ae763253ba535e05eaa8cee
Shubham Singh <shubham88fru@yahoo.com>
2023-03-16 19:51:28 +0530

    lowest common ancestor in bst

---
+++ src/main/java/swd/bst/LowestCommonAncestorOfBST.java
@@ -0,0 +1,18 @@
+package swd.bst;
+
+public class LowestCommonAncestorOfBST {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        TreeNode currNode = root;
+        while (currNode != null) {
+            //go left if both nodes are in smaller
+            if (p.val < currNode.val && q.val < currNode.val) currNode = currNode.left;
+                //right if both nodes are larger.
+            else if (p.val > currNode.val && q.val > currNode.val) currNode = currNode.right;
+
+                //finally the moment both nodes are on other side of root, that node will be our ans.
+            else return currNode;
+        }
+
+        return currNode;
+    }
+}

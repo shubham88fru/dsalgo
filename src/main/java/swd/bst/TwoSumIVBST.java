c7d24f312e400129f6307a39e6dbc78a82f048da
Shubham Singh <shubham88fru@yahoo.com>
2023-03-17 11:08:23 +0530

    two sum bst

---
+++ src/main/java/swd/bst/TwoSumIVBST.java
@@ -0,0 +1,24 @@
+package swd.bst;
+
+import java.util.HashMap;
+import java.util.Map;
+
+//@link - https://leetcode.com/problems/two-sum-iv-input-is-a-bst/description/
+public class TwoSumIVBST {
+    public boolean findTarget(TreeNode root, int k) {
+        Map<Integer, Integer> seenmap = new HashMap<>();
+        return pairPresent(root, k, seenmap);
+    }
+
+    private boolean pairPresent(TreeNode root, int k, Map<Integer, Integer> seenmap) {
+        if (root == null) return false;
+
+        if (seenmap.containsKey(k - root.val)) return true;
+        if (!seenmap.containsKey(root.val)) seenmap.put(root.val, 1);
+        boolean left = pairPresent(root.left, k, seenmap);
+
+        if (left) return true;
+        boolean right = pairPresent(root.right, k, seenmap);
+        return right;
+    }
+}

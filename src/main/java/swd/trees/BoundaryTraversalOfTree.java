12c4bf26bfac96e0f0780f3b2033a5385f066b0c
Shubham Singh <shubham88fru@yahoo.com>
2023-05-02 13:26:55 +0530

    boundary traversal of tree

---
+++ src/main/java/swd/trees/BoundaryTraversalOfTree.java
@@ -0,0 +1,73 @@
+package swd.trees;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//@link - https://practice.geeksforgeeks.org/problems/boundary-traversal-of-binary-tree/1?utm_source=gfg&utm_medium=article&utm_campaign=bottom_sticky_on_article
+public class BoundaryTraversalOfTree {
+    ArrayList<Integer> boundary(GfgNode node) {
+        ArrayList<Integer> ans = new ArrayList<>();
+
+        //if root node is a leaf, it will get added
+        //twice by addLeafNodes call, so prevent that.
+        if (!isLeafNode(node))
+            ans.add(node.data);
+
+        //add all left boundary.
+        addLeftBoundary(node.left, ans);
+
+        //add all leaf node.
+        addLeafNodes(node, ans);
+
+        //add all right node.
+        addRightBoundary(node.right, ans);
+
+        return ans;
+    }
+
+    private void addLeftBoundary(GfgNode node, List<Integer> ans) {
+        if (node == null) return;
+
+        //add only if not a leaf node.
+        if (!isLeafNode(node)) {
+            ans.add(node.data);
+        }
+
+        if (node.left != null)
+            addLeftBoundary(node.left, ans);
+        else if (node.right != null)
+            addLeftBoundary(node.right, ans);
+    }
+
+    private void addRightBoundary(GfgNode node, List<Integer> ans) {
+        if (node == null) return;
+
+        if (node.right != null)
+            addRightBoundary(node.right, ans);
+
+        else if (node.left != null)
+            addRightBoundary(node.left, ans);
+
+        //Traverse backwards because its right boundary
+        //and add only if not a leaf node.
+        if (!isLeafNode(node)) {
+            ans.add(node.data);
+        }
+
+    }
+
+    private void addLeafNodes(GfgNode node, List<Integer> ans) {
+        if (node == null) return;
+
+        if (isLeafNode(node)) {
+            ans.add(node.data);
+        }
+
+        addLeafNodes(node.left, ans);
+        addLeafNodes(node.right, ans);
+    }
+
+    private boolean isLeafNode(GfgNode node) {
+        return node.left == null && node.right == null;
+    }
+}

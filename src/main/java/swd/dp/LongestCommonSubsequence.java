4600786564e0f757f43a1d8280e336a654b8ae09
Shubham Singh <shubham88fru@yahoo.com>
2023-06-09 08:07:44 +0530

    longest common subsequence

--- src/main/java/swd/dp/LongestCommonSubsequence.java
+++ src/main/java/swd/dp/LongestCommonSubsequence.java
@@ -5,15 +5,43 @@ import java.util.Map;
 
 //@link - https://leetcode.com/problems/longest-common-subsequence/
 public class LongestCommonSubsequence {
+    //1) Bottom-up approach.
     public int longestCommonSubsequence(String s1, String s2) {
+        return lengthOfLCS(s1, s2);
+    }
+
+    private int lengthOfLCS(String s1, String s2) {
+        int m = s1.length();
+        int n = s2.length();
+
+        int[][] dp = new int[m+1][n+1];
+
+        for (int i=1; i < m+1; i++) {
+            for (int j=1; j < n+1; j++) {
+                if (s1.charAt(i-1) == s2.charAt(j-1))
+                    dp[i][j] = 1 + dp[i-1][j-1];
+
+                else
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+            }
+        }
+
+        return dp[m][n];
+    }
+
+
+
+
+    //2) Top-down approach.
+    public int longestCommonSubsequence2(String s1, String s2) {
         // For a string of length `n`:
         // No. of substrings = n*(n+1)/2 (substrings don't include empty string). Neither deletion/nor order change allowed.
         // No. of subsequences = 2^n (subsequences include empty string). Deletion of chars allowed, but can't change order.
 
-        return lengthOfLCS(s1, s2, 0, 0, s1.length(), s2.length(), new HashMap<String, Integer>());
+        return lengthOfLCS2(s1, s2, 0, 0, s1.length(), s2.length(), new HashMap<String, Integer>());
     }
 
-    private int lengthOfLCS(String s1, String s2, int firstStringIndex, int secondStringIndex, int s1Length, int s2Length, Map<String, Integer> memo) {
+    private int lengthOfLCS2(String s1, String s2, int firstStringIndex, int secondStringIndex, int s1Length, int s2Length, Map<String, Integer> memo) {
         // Only empty string (length 0) is the possible ans.
         if (firstStringIndex >= s1Length || secondStringIndex >= s2Length) return 0;
 
@@ -22,10 +50,10 @@ public class LongestCommonSubsequence {
         if (memo.containsKey(key)) return memo.get(key);
 
         if (s1.charAt(firstStringIndex) == s2.charAt(secondStringIndex)) {
-            ans = 1 + lengthOfLCS(s1, s2, firstStringIndex+1, secondStringIndex+1, s1Length, s2Length, memo);
+            ans = 1 + lengthOfLCS2(s1, s2, firstStringIndex+1, secondStringIndex+1, s1Length, s2Length, memo);
         } else {
-            int a = lengthOfLCS(s1, s2, firstStringIndex, secondStringIndex+1, s1Length, s2Length, memo);
-            int b = lengthOfLCS(s1, s2, firstStringIndex+1, secondStringIndex, s1Length, s2Length, memo);
+            int a = lengthOfLCS2(s1, s2, firstStringIndex, secondStringIndex+1, s1Length, s2Length, memo);
+            int b = lengthOfLCS2(s1, s2, firstStringIndex+1, secondStringIndex, s1Length, s2Length, memo);
             ans = Math.max(a, b);
         }
 

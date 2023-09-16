2acd75694dad375c26464060f3ca68c47b035396
Shubham Singh <shubham88fru@yahoo.com>
2023-09-15 19:49:01 -0700

    src/main/java/strvr/recursionandbacktracking/MColoring.java

--- src/main/java/strvr/recursion/CombinationSumIV.java
+++ src/main/java/strvr/recursion/CombinationSumIV.java
@@ -1,2 +1,29 @@
-package strvr.recursion;public class CombinationSumIV {
+package strvr.recursion;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CombinationSumIV {
+    public int combinationSum4(int[] nums, int target) {
+        List<List<Integer>> combs = new ArrayList<>();
+        numCombs(0, nums, combs, new ArrayList<>(), target);
+        return combs.size();
+    }
+
+    private void numCombs(int curr, int[] nums, List<List<Integer>> combs, List<Integer> comb, int target) {
+        if (target == 0) {
+            combs.add(new ArrayList<>(comb));
+            return;
+        }
+
+        if (curr >= nums.length) return;
+
+        if (nums[curr] <= target) {
+            comb.add(nums[curr]);
+            numCombs(curr, nums, combs, comb, target-nums[curr]);
+            comb.remove(comb.size()-1);
+        }
+
+        numCombs(curr+1, nums, combs, comb, target);
+    }
 }

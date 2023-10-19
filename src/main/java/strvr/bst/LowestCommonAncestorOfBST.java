3c9ba1c49fc1476750489ad4b6afa56af914b634
Shubham Singh <shubham88fru@yahoo.com>
2023-10-18 19:36:12 -0700

    largest common ancestor in bst

--- src/main/java/strvr/bst/LowestCommonAncestorOfBST.java
+++ src/main/java/strvr/bst/LowestCommonAncestorOfBST.java
@@ -1,7 +1,7 @@
-package swd.bst;
-
+package strvr.bst;
 
 //@link - https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/description/
+//@strvr - https://www.youtube.com/watch?v=cX_kPV_foZc&ab_channel=takeUforward
 public class LowestCommonAncestorOfBST {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         TreeNode currNode = root;

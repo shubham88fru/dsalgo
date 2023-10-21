35d8a420b4e118e3170bccd149e6b879d2262529
Shubham Singh <shubham88fru@yahoo.com>
2023-10-21 15:55:41 -0700

    ceil in a bst

--- src/main/java/strvr/bst2/CeilInBST.java
+++ src/main/java/strvr/bst2/CeilInBST.java
@@ -1,18 +1,21 @@
-package basics.bst;
+package strvr.bst2;
+
 
 //Given a bst and a key, find the
 //closest larger or equal value in bst
 //to the given key.
+//@link - https://practice.geeksforgeeks.org/problems/implementing-ceil-in-bst/0
+//@strvr - https://www.youtube.com/watch?v=KSsk8AhdOZA&ab_channel=takeUforward
 public class CeilInBST {
 
     //T:O(H), S:O(1)
-    Node findCeil(Node root, int key) {
+    GfgNode findCeil(GfgNode root, int key) {
         if (root == null) return null;
-        Node curr = root;
-        Node res = null;
+        GfgNode curr = root;
+        GfgNode res = null;
         while (curr != null) {
-            if (curr.key == key) return curr;
-            else if (key < curr.key) {
+            if (curr.data == key) return curr;
+            else if (key < curr.data) {
                 //when we see a node greater than
                 //key, record it. This is our last closest
                 //seen value.
@@ -29,21 +32,21 @@ public class CeilInBST {
         CeilInBST ceilInBST =
                 new CeilInBST();
 
-        Node root = new Node(10);
-        root.left = new Node(5);
-        root.right = new Node(15);
-        root.right.left = new Node(12);
-        root.right.right = new Node(30);
+        GfgNode root = new GfgNode(10);
+        root.left = new GfgNode(5);
+        root.right = new GfgNode(15);
+        root.right.left = new GfgNode(12);
+        root.right.right = new GfgNode(30);
 
-        Node root2 = new Node(50);
-        root2.left = new Node(30);
-        root2.left.left = new Node(20);
-        root2.left.right = new Node(40);
-        root2.right = new Node(70);
-        root2.right.left = new Node(60);
-        root2.right.left.left = new Node(55);
-        root2.right.left.right = new Node(65);
-        root2.right.right = new Node(80);
+        GfgNode root2 = new GfgNode(50);
+        root2.left = new GfgNode(30);
+        root2.left.left = new GfgNode(20);
+        root2.left.right = new GfgNode(40);
+        root2.right = new GfgNode(70);
+        root2.right.left = new GfgNode(60);
+        root2.right.left.left = new GfgNode(55);
+        root2.right.left.right = new GfgNode(65);
+        root2.right.right = new GfgNode(80);
 
         System.out.println(ceilInBST
                 .findCeil(root, 14));

94934090e1b92533bc708a2752fc6190df8f6833
Shubham Singh <shubham88fru@yahoo.com>
2023-10-21 16:30:51 -0700

    kth largest and smallest elements in bst

---
+++ src/main/java/strvr/bst2/KthLargestElementInBST.java
@@ -0,0 +1,26 @@
+package strvr.bst2;
+
+//@link - https://practice.geeksforgeeks.org/problems/kth-largest-element-in-bst/1
+//@strvr - https://takeuforward.org/data-structure/kth-largest-smallest-element-in-binary-search-tree/
+public class KthLargestElementInBST {
+    public int kthLargest(GfgNode root,int K){
+        int[] karr = {K};
+        return findKthLargest(root, karr);
+    }
+
+    //doing reverse inorder. will give the elements in sorted manner
+    //from high to low.
+    private int findKthLargest(GfgNode root, int[] karr) {
+        if (root == null) return -1;
+
+        int right = findKthLargest(root.right, karr);
+        if (karr[0] == 1 && right == -1) {
+            return root.data;
+        } else karr[0] -= 1;
+
+        if (right != -1) return right;
+
+        int left = findKthLargest(root.left, karr);
+        return left;
+    }
+}
--- src/main/java/strvr/bst2/KthSmallestElementInBST.java
+++ src/main/java/strvr/bst2/KthSmallestElementInBST.java
@@ -9,16 +9,28 @@ public class KthSmallestElementInBST {
         return findKthSmallest(root, karr);
     }
 
+    //exploiting a property of bst that
+    //the inorder traversal of bst is sorted.
     private int findKthSmallest(TreeNode root, int[] karr) {
         if (root == null) return -1;
 
+        //left
         int left = findKthSmallest(root.left, karr);
+
+        //root
+        //when processing a node, we know that its the next
+        //element in a sorted manner, so we keep decrementing
+        //our counter (which was initialized to k),
+        //till it becomes 1.
+        //Although, I'm not sure why we did this left == -1 and left != -1 check.
+        //Might have to check swd video again.
         if (karr[0] == 1 && left == -1) {
             return root.val;
         } else karr[0] -= 1;
 
         if (left != -1) return left;
 
+        //right
         int right = findKthSmallest(root.right, karr);
         return right;
     }

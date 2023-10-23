ef4b60ba803f9366306a512ad0e26ea2eeb232c7
Shubham Singh <shubham88fru@yahoo.com>
2023-10-22 17:22:34 -0700

    max sum bst in bt

--- src/main/java/strvr/bst2/MaxSumBSTInBT.java
+++ src/main/java/strvr/bst2/MaxSumBSTInBT.java
@@ -1,30 +1,31 @@
 package strvr.bst2;
 
-//@link - https://practice.geeksforgeeks.org/problems/largest-bst/1
-//@strvr - https://www.youtube.com/watch?v=X0oXMdtUDwo&ab_channel=takeUforward
-public class LargestBSTInBTStrvr {
-    int largestBst(GfgNode root){
-        // Write your code here
-        return maxBST(root).size;
-
+//@link - https://leetcode.com/problems/maximum-sum-bst-in-binary-tree/
+//@strvr - not solved, but on very similar lines to Largest bst in bt.
+public class MaxSumBSTInBT {
+    int maxSum = 0;
+    public int maxSumBST(TreeNode root) {
+        bstWithMaxSum(root);
+        return maxSum > 0 ? maxSum: 0;
     }
 
-    private BSTData maxBST(GfgNode root) {
+    private BSTData bstWithMaxSum(TreeNode root) {
         if (root == null) return new BSTData(0, Integer.MAX_VALUE, Integer.MIN_VALUE, 0);
-        //if (root.left == null && root.right == null) return new BSTData(1, root.val, root.val);
 
-        BSTData left = maxBST(root.left);
-        BSTData right = maxBST(root.right);
+        BSTData left = bstWithMaxSum(root.left);
+        BSTData right = bstWithMaxSum(root.right);
 
         //if curr node's value is more than max in left
         //and less than min in right, and we know that left and right
         //are bst's because there min and max are marked accordingly (read comments below),
         //then left, right and curr will form a bst too.
-        if (root.data > left.max && root.data < right.min) {
+        if (root.val > left.max && root.val < right.min) {
             //left,right and curr form a valid bst.
             int size = 1+left.size+right.size; //left's size + right's size + curr node.
-            return new BSTData(size, Math.min(root.data, left.min),
-                    Math.max(root.data, right.max), 0);
+            int sum = left.sum + right.sum + root.val;
+            maxSum = Math.max(maxSum, sum);
+            return new BSTData(size, Math.min(root.val, left.min),
+                    Math.max(root.val, right.max), sum);
         } else {
             //left, right and curr don't form a valid bst.
             //In this case, we'll still keep track for the size of valid
@@ -39,22 +40,7 @@ public class LargestBSTInBTStrvr {
             //parent' won't be able to make a bst with such a subtree.
             //Note: we set the min and max to lowest and highest to mark this tree invalid bst,
             //but we still carry the max bst size that we have seen till now.
-            return new BSTData(Math.max(left.size, right.size), Integer.MIN_VALUE, Integer.MAX_VALUE, 0);
+            return new BSTData(Math.max(left.size, right.size), Integer.MIN_VALUE, Integer.MAX_VALUE, Math.max(left.sum, right.sum));
         }
-    }
-}
-
-class BSTData {
-    int size;
-    int min;
-    int max;
-
-    int sum;
-
-    public BSTData(int size, int min, int max, int sum) {
-        this.size = size;
-        this.min = min;
-        this.max = max;
-        this.sum = sum;
     }
 }

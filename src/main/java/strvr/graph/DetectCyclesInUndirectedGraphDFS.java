809296e3015b34f6dfb09889a91cffc93cf3de41
Shubham Singh <shubham88fru@yahoo.com>
2023-10-23 20:06:49 -0700

    detect cyles in undirected graph

--- src/main/java/strvr/graph/DetectCyclesInUndirectedGraphDFS.java
+++ src/main/java/strvr/graph/DetectCyclesInUndirectedGraphDFS.java
@@ -1,5 +1,44 @@
 package strvr.graph;
 
-public class DetectCyclesInUndirectedGraphDFS {
+import java.util.ArrayList;
 
+//@link - https://practice.geeksforgeeks.org/problems/detect-cycle-in-an-undirected-graph/1
+//@strvr - https://www.youtube.com/watch?v=zQ3zgFypzX4&ab_channel=takeUforward
+public class DetectCyclesInUndirectedGraphDFS {
+    /*
+        idea is exactly the same as detecting cycle in undirected graph using BFS
+    * */
+    public boolean isCycle(int V, ArrayList<ArrayList<Integer>> adj) {
+        boolean[] visited = new boolean[V];
+        for (int i=0; i<visited.length; i++) {
+            if (!visited[i]) {
+                Vertex v = new Vertex(i, -1);
+                if (dfs(v, adj, visited)) return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean dfs(Vertex currVertex, ArrayList<ArrayList<Integer>> adj, boolean[] visited) {
+
+        visited[currVertex.vertex] = true;
+        ArrayList<Integer> neighbours = adj.get(currVertex.vertex);
+        for (int neighbour: neighbours) {
+            if (!visited[neighbour]) {
+                if (dfs(new Vertex(neighbour, currVertex.vertex), adj, visited)) return true;
+            } else if (neighbour != currVertex.parent) return true;
+        }
+
+        return false;
+    }
+}
+
+class Vertex {
+    int vertex;
+    int parent;
+
+    public Vertex(int vertex, int parent) {
+        this.vertex = vertex;
+        this.parent = parent;
+    }
 }

5a8bdda3f4762bbd58c9915f300ca223cd379859
Shubham Singh <shubham88fru@yahoo.com>
2023-10-25 06:49:46 -0700

    detect cyles in directed graph dfs

--- src/main/java/strvr/graph/DetectCyclesInDirectedGraphDFS.java
+++ src/main/java/strvr/graph/DetectCyclesInDirectedGraphDFS.java
@@ -3,7 +3,24 @@ package strvr.graph;
 import java.util.ArrayList;
 import java.util.*;
 
+/*
+*  Although it might seem that the same algorithm used for detecting cycles in
+*   undirected might work for directed graphs as well, but there are some cases
+*  where it would fail. E.g. if a graph has a component like below, the algo for
+* detecting cycle in undirected graph would say it has a cycle, while in reality,
+* it doesn't.
+*           >4
+*         /     \
+*        /       \
+*    ----3        >5
+*         \      /
+*          \   /
+*            >6
+* */
+
+
 //@link - https://practice.geeksforgeeks.org/problems/detect-cycle-in-a-directed-graph/1
+//@strvr - https://www.youtube.com/watch?v=uzVUw90ZFIg&ab_channel=takeUforward
 public class DetectCyclesInDirectedGraphDFS {
     //This is some nasty code, which not even swd was confident about.
     //check day 4 video on graphs if need refresher.
@@ -15,11 +32,11 @@ public class DetectCyclesInDirectedGraphDFS {
         //in the current recursive call. For a graph to be cyclic,
         //we should be revisiting a already visited node **and** also
         //the node should be in the current recursive stack.
-        Set<Integer> recursiveStack = new HashSet<>();
+        Set<Integer> currRecursiveStack = new HashSet<>();
 
         for (int currentVertex = 0; currentVertex < V; currentVertex++) {
             if (!visited[currentVertex]){
-                if (hasCycle(adj, currentVertex, recursiveStack, visited)) {
+                if (hasCycle(adj, currentVertex, currRecursiveStack, visited)) {
                     return true;
                 }
             }
@@ -28,90 +45,36 @@ public class DetectCyclesInDirectedGraphDFS {
         return false;
     }
 
+    //Try to follow this code on the diagram above, for
+    //understanding better why we need the currRecursiveStack.
     private boolean hasCycle(ArrayList<ArrayList<Integer>> adj,
-                             int currentVertex, Set<Integer> recursiveStack, boolean[] visited) {
+                             int currentVertex, Set<Integer> currRecursiveStack, boolean[] visited) {
 
         visited[currentVertex] = true;
-        recursiveStack.add(currentVertex);
+        currRecursiveStack.add(currentVertex);
 
         ArrayList<Integer> neighbours = adj.get(currentVertex);
 
         for (int neighbour: neighbours) {
             if (!visited[neighbour]) {
-                if (hasCycle(adj, neighbour, recursiveStack, visited)) {
+                if (hasCycle(adj, neighbour, currRecursiveStack, visited)) {
                     return true;
                 }
             }
 
-            if (recursiveStack.contains(neighbour)) {
+            //if the node has been visited in the dfs means,
+            //while traversing from some parent, we are encountering the current node
+            //for the second time.
+            if (currRecursiveStack.contains(neighbour)) {
                 return true;
             }
         }
 
-        recursiveStack.remove(currentVertex);
+        //remove the curr node from recursive stack when we back track.
+        //Note: we don't remove like this from the visited array.
+        currRecursiveStack.remove(currentVertex);
         return false;
     }
 
-    /*** 2) SWD Original Soln - Doesn't work ***/
-    // public boolean isCyclic(int V, ArrayList<ArrayList<Integer>> adj) {
-    //     boolean[] visited = new boolean[V];
-    //     Set<Integer> recursiveStack = new HashSet<>();
 
-    //     for (int currentVertex = 0; currentVertex < V; currentVertex++) {
-    //         if (!visited[currentVertex]){
-    //             if (hasCycle(adj, currentVertex, recursiveStack, visited)) {
-    //                 return true;
-    //             }
-    //         }
-    //     }
-
-    //     return false;
-    // }
-
-    // private boolean hasCycle(ArrayList<ArrayList<Integer>> adj,
-    //                             int currentVertex, Set<Integer> recursiveStack, boolean[] visited) {
-
-    //     visited[currentVertex] = true;
-    //     recursiveStack.add(currentVertex);
-
-    //     ArrayList<Integer> neighbours = adj.get(currentVertex);
-
-    //     for (int neighbour: neighbours) {
-    //         if (!visited[neighbour]) {
-    //             return hasCycle(adj, neighbour, recursiveStack, visited);
-    //         }
-    //         return recursiveStack.contains(neighbour);
-    //     }
-
-    //     recursiveStack.remove(currentVertex);
-    //     return false;
-    // }
-
-    /*** 3) My Soln - Doesn't work ***/
-    // public boolean isCyclic(int V, ArrayList<ArrayList<Integer>> adj) {
-    //     // code here
-    //     boolean[] visited = new boolean[adj.size()];
-
-    //     for (int i=0; i<adj.size(); i++) {
-    //         boolean ans = dfs(adj, i, new boolean[adj.size()]);
-    //         if (ans) return true;
-    //     }
-
-    //     return false;
-    // }
-
-    // private boolean dfs(ArrayList<ArrayList<Integer>> adj, int curr, boolean[] visited) {
-    //     if (visited[curr]) return true;
-
-    //     visited[curr] = true;
-    //     ArrayList<Integer> neighbours = adj.get(curr);
-
-    //     boolean ans = false;
-    //     for (int neighbour: neighbours) {
-    //         ans = ans || dfs(adj, neighbour, visited);
-    //         if (ans) return true;
-    //     }
-
-    //     return ans;
-    // }
 }

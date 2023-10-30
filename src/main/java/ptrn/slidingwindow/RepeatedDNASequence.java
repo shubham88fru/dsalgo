100f2ae4374ed57bf5bf8f72ad2233ce77189a23
Shubham Singh <shubham88fru@yahoo.com>
2023-10-30 14:36:53 -0700

    repeated dna sequence

--- src/main/java/ptrn/slidingwindow/RepeatedDNASequence.java
+++ src/main/java/ptrn/slidingwindow/RepeatedDNASequence.java
@@ -1,9 +1,6 @@
 package ptrn.slidingwindow;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 //@link - https://leetcode.com/problems/repeated-dna-sequences/
 public class RepeatedDNASequence {
@@ -18,23 +15,24 @@ public class RepeatedDNASequence {
         int i = 0;
         int j = 9;
 
+        //final ans.
+        Set<String> ans = new HashSet<>();
+
         //till end of window
         while (j < s.length()) {
             //take substring of len 10.
             String sequence = s.substring(i, j+1);
             //put in freq map and increase count.
             freq.put(sequence, freq.getOrDefault(sequence, 0)+1);
+
+            //if frequence more than 1, add to set (set - so we don't store duplicates)
+            if (freq.get(sequence) > 1) ans.add(sequence);
+
             //slide the window.
             i += 1;
             j += 1;
         }
 
-        //loop through map and collate
-        //all sequences that which occured more than once.
-        List<String> ans = new ArrayList<>();
-        for (Map.Entry<String, Integer> entry: freq.entrySet()) {
-            if (entry.getValue() > 1) ans.add(entry.getKey());
-        }
-        return ans;
+        return new ArrayList<>(ans);
     }
 }

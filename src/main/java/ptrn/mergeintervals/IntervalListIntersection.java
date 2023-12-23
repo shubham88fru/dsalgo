933bfc51c974af7aeeb223732880a878cfec419d
Shubham Singh <shubham88fru@yahoo.com>
2023-12-23 09:55:39 -0700

    comments

--- src/main/java/ptrn/mergeintervals/IntervalListIntersection.java
+++ src/main/java/ptrn/mergeintervals/IntervalListIntersection.java
@@ -19,13 +19,15 @@ public class IntervalListIntersection {
         List<int[]> intersections = new ArrayList<>();
 
         while (f < fl && s < sl) {
-            /** May need to visit multiple times to wrap my head. **/
             //out of f and s, whichever starts the most later.
             int start = Math.max(firstList[f][0], secondList[s][0]);
 
             //out of f and s, whichever ends the most earlier.
             int end = Math.min(firstList[f][1], secondList[s][1]);
 
+            //an intersection of two segments happens only when
+            //the latest start is before the earliest end.
+            //(image an internsection of two ranges on number line)
             if (start <= end) {
                 intersections.add(new int[]{start, end});
             }

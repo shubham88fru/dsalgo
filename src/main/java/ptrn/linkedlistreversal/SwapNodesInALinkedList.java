4ee0658753c1b3c34c560d822c294e749551ff6a
Shubham Singh <shubham88fru@yahoo.com>
2023-12-26 13:07:58 -0700

    swapping nodes

--- src/main/java/ptrn/linkedlistreversal/SwapNodesInALinkedList.java
+++ src/main/java/ptrn/linkedlistreversal/SwapNodesInALinkedList.java
@@ -1,8 +1,43 @@
-package swd.linkedlist;
+package ptrn.linkedlistreversal;
 
 //@link - https://leetcode.com/problems/swapping-nodes-in-a-linked-list/description/
+//@check - https://www.educative.io/module/page/Z4JLg2tDQPVv6QjgO/10370001/4976190424350720/5480851682099200
 public class SwapNodesInALinkedList {
     public ListNode swapNodes(ListNode head, int k) {
+        return swapNodes2(head, k);
+        //return swapNodes1(head, k);
+    }
+
+    private ListNode swapNodes2(ListNode head, int k) {
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+        ListNode p1 = dummy;
+        ListNode p2 = dummy;
+
+        //to find kth node from front.
+        while (k > 0) {
+            p2 = p2.next;
+            k -= 1;
+        }
+        ListNode kthFromFront = p2;
+
+        //two pointer approach to
+        //find kth node from the end.
+        while (p2 != null) {
+            p1 = p1.next;
+            p2 = p2.next;
+        }
+        ListNode kthFromEnd = p1;
+
+        //swap the values.
+        int temp = kthFromEnd.val;
+        kthFromEnd.val = kthFromFront.val;
+        kthFromFront.val = temp;
+
+        return head;
+    }
+
+    private ListNode swapNodes1(ListNode head, int k) {
         //get nth node from end.
         ListNode nthFromEnd = getNthFromEnd(head, k);
 

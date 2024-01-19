5e165480f493dc0de1424002eca8c528696fe9dc
Shubham Singh <shubham88fru@yahoo.com>
2024-01-19 09:57:15 -0700

    kth largest element in a stream

--- src/main/java/ptrn/topkelements/KthLargestElementInAStream.java
+++ src/main/java/ptrn/topkelements/KthLargestElementInAStream.java
@@ -3,13 +3,38 @@ package ptrn.topkelements;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.PriorityQueue;
 
-//@link -
-//@check -
-
+//@link - https://leetcode.com/problems/kth-largest-element-in-a-stream/
+//@check - https://www.educative.io/module/page/Z4JLg2tDQPVv6QjgO/10370001/4976190424350720/6600090295992320
 //1) Optimal approach
 public class KthLargestElementInAStream {
+    int k;
+    /**
+     We’ll implement a min-heap of size k.
+     In a min-heap, the smallest number is always at the top.
+     We’ll use this property to design a solution that
+     ensures that in a min-heap with k elements, the kth largest
+     element is always at the top of the heap.
+     */
+    PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+    public KthLargestElementInAStream(int k, int[] nums) {
+        this.k = k;
 
+        for (int num : nums) {
+            add(num);
+        }
+    }
+
+    public int add(int val) {
+        if (minHeap.size() < k) {
+            minHeap.add(val);
+        } else if (minHeap.peek() <= val) {
+            minHeap.remove();
+            minHeap.add(val);
+        }
+        return minHeap.peek();
+    }
 }
 
 //2) Brute force approach.

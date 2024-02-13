afc4ac1f725b235d914c6f411a7d540e60012fe7
Shubham Singh <shubham88fru@yahoo.com>
2024-02-13 07:18:09 -0700

    flood fill

--- src/main/java/ptrn/backtracking/FloodFill.java
+++ src/main/java/ptrn/backtracking/FloodFill.java
@@ -1,8 +1,15 @@
 package ptrn.backtracking;
 
 //@link - https://leetcode.com/problems/flood-fill/description/
+//@check - https://www.educative.io/module/page/Z4JLg2tDQPVv6QjgO/10370001/4976190424350720/4960459742248960
 public class FloodFill {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        //return floodFill1(image, sr, sc, color);
+        return floodFill2(image, sr, sc, color);
+    }
+
+    //1) swd solution
+    private int[][] floodFill1(int[][] image, int sr, int sc, int color) {
         int m = image.length;
         int n = image[0].length;
 
@@ -39,5 +46,33 @@ public class FloodFill {
 
         //left
         dfs(image, m, n, i, j-1, origcolor, color);
+    }
+
+    //2) Edctv soln.
+    private int[][] floodFill2(int[][] image, int sr, int sc, int color) {
+        //already flood-filled.
+        if (image[sr][sc] == color) return image;
+
+        int ogColor = image[sr][sc];
+        image[sr][sc] = color;
+        dfs2(image, sr, sc, ogColor, color);
+
+        return image;
+    }
+
+    private void dfs2(int[][] image, int row, int col, int ogColor, int target) {
+        int[][] adjacentCells = { {0, 1}, { 1, 0 }, {-1, 0}, {0, -1} };
+        int m = image.length;
+        int n = image[0].length;
+
+        for (int[] adjacent: adjacentCells) {
+            int i = row + adjacent[0];
+            int j = col + adjacent[1];
+
+            if (i<m && i>=0 && j<n && j>=0 && image[i][j] == ogColor) {
+                image[i][j] = target;
+                dfs2(image, i, j, ogColor, target);
+            }
+        }
     }
 }

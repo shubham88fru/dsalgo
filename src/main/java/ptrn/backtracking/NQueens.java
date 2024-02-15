b44a7d09d5bd77a8f94f87ed22108a2ad76491a5
Shubham Singh <shubham88fru@yahoo.com>
2024-02-15 11:39:18 -0700

    nqueens

--- src/main/java/ptrn/backtracking/NQueens.java
+++ src/main/java/ptrn/backtracking/NQueens.java
@@ -1,13 +1,18 @@
 package ptrn.backtracking;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 //@link - https://leetcode.com/problems/n-queens/solutions/
 //@strvr - https://takeuforward.org/data-structure/n-queen-problem-return-all-distinct-solutions-to-the-n-queens-puzzle/
 //@check - https://www.educative.io/module/page/Z4JLg2tDQPVv6QjgO/10370001/4976190424350720/5452005473779712
 public class NQueens {
     public List<List<String>> solveNQueens(int n) {
+        return solveRecursive(n);
+        //return solveIterative(n);
+    }
+
+    //1) Recursive solution.
+    private List<List<String>> solveRecursive(int n) {
         //initialize a board with dots.
         char[][] board = new char[n][n];
         for (int i = 0; i<n; i++) {
@@ -35,11 +40,8 @@ public class NQueens {
 
         List<List<String>> allMoves = new ArrayList<>();
         for (int col = 0; col < size; col++) {
-            //if position is valid..
             if (isValidMove(board, i, col, size)) {
-                //place the queen.
                 board[i][col] = 'Q';
-                //and check further from this state.
                 List<List<String>> listOfMoves = placeQueens(board, i+1, size);
 
                 if (listOfMoves.size() != 0) {
@@ -54,13 +56,79 @@ public class NQueens {
         return allMoves;
     }
 
+    //2) My nasty iterative solution using stacks.
+    //check edctv for a cleaner iterative solution (maybe?)
+    private List<List<String>> solveIterative(int n) {
+
+        List<List<String>> ans = new ArrayList<>();
+        if (n == 1) ans.add(Arrays.asList("Q"));
+        Deque<Integer> stack = new ArrayDeque<>();
+        char[][] board = new char[n][n];
+        StringBuffer dotStr = new StringBuffer();
+        for (int i = 0; i<n; i++) {
+            dotStr.append(".");
+            for (int j=0; j<n; j++) {
+                board[i][j] = '.';
+            }
+        }
+
+        int row = 0;
+        board[0][0] = 'Q';
+        stack.addFirst(0);
+        stack.addFirst(-1);
+        row = 1;
+
+        while (!stack.isEmpty()) {
+            int j = stack.peekFirst();
+            if (j >= 0)
+                board[row][j] = '.';
+            for (j=j+1; j<n; j++) {
+                if (isValidMove(board, row, j, n)) {
+                    board[row][j] = 'Q';
+                    stack.removeFirst();
+                    stack.addFirst(j);
+                    stack.addFirst(-1);
+                    row += 1;
+                    break;
+                }
+
+
+            }
+
+            if (j >= n) {
+                stack.removeFirst();
+                row -= 1;
+            }
+            if (row >= n) {
+                stack.removeFirst();
+                row -= 1;
+                List<Integer> lst = new ArrayList<>(stack);
+                List<String> config = new ArrayList<>();
+                for (int idx: lst) {
+                    StringBuffer sb = new StringBuffer(dotStr);
+                    sb.setCharAt(idx, 'Q');
+                    config.add(sb.toString());
+                }
+                ans.add(config);
+            }
+        }
+        return ans;
+
+    }
+
 
     //a move is valid if curr value is not present in current row, current col
     //and submatrix.
     private boolean isValidMove(char[][] board, int i, int j, int size) {
-        return isRowValid(board, i, size) &&
+
+        return inBounds(board, i, j, size) &&
+                isRowValid(board, i, size) &&
                 isColumnValid(board, j, size) &&
                 isDiagonalValid(board, i, j, size);
+    }
+
+    private boolean inBounds(char[][] board, int i, int j, int size) {
+        return (i >= 0 && j >= 0 && i < size && j < size);
     }
 
     //checks if value is present anywhere in given row.

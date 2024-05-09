b01c177e9569e871adf74a8c9c45e811c359c811
shubham88fru <shubham88fru@yahoo.com>
2024-05-09 07:39:16 -0700

    right view of bt

--- src/main/java/ptrn/trees/dfs/RightViewOfBT.java
+++ src/main/java/ptrn/trees/dfs/RightViewOfBT.java
@@ -1,11 +1,10 @@
-package strvr.binarytree;
+package ptrn.trees.dfs;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
-//@link - https://practice.geeksforgeeks.org/problems/right-view-of-binary-tree/1
+//@link - https://leetcode.com/problems/binary-tree-right-side-view/description/
+        //https://practice.geeksforgeeks.org/problems/right-view-of-binary-tree/1
+//@check - https://www.educative.io/module/page/Z4JLg2tDQPVv6QjgO/10370001/4976190424350720/4676245086011392
 //@strvr - https://takeuforward.org/data-structure/right-left-view-of-binary-tree/
 public class RightViewOfBT {
     ArrayList<Integer> rightView(GfgNode node) {
@@ -28,7 +27,7 @@ public class RightViewOfBT {
     private void traverseForRightView(GfgNode root, List<Integer> ans, int height, Map<Integer, Integer> heightMap) {
         if (root == null) return;
 
-        //If no node see at current height till now,
+        //If no node seen at current height till now,
         // then visible in left view.
         if (!heightMap.containsKey(height)) {
             heightMap.put(height, height);
@@ -45,5 +44,36 @@ public class RightViewOfBT {
         //consist of first occurrences of right most child.
         traverseForRightView(root.right, ans, height + 1, heightMap);
         traverseForRightView(root.left, ans, height + 1, heightMap);
+    }
+
+    //3) My soln using BFS
+    private List<Integer> bfs(TreeNode root) {
+        List<Integer> ans = new ArrayList<>();
+        if (root == null) return ans;
+        Deque<TreeNode> q = new ArrayDeque<>();
+        q.addLast(root);
+        while (!q.isEmpty()) {
+            int els = q.size();
+            while (els != 0) {
+                TreeNode curr = q.removeFirst();
+                if (curr.left != null) q.addLast(curr.left);
+                if (curr.right != null) q.addLast(curr.right);
+                //at each level, the last element (i.e. rightmost)
+                //will be part of the right side view.
+                els -= 1;
+                if (els == 0) ans.add(curr.val);
+            }
+        }
+        return ans;
+    }
+}
+
+class GfgNode {
+    int data;
+    GfgNode left, right;
+
+    GfgNode(int item) {
+        data = item;
+        left = right = null;
     }
 }

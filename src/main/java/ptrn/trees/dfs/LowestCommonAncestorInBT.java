5fb23ab5f8c4d7d59f0a029ed9c1b05d5cf00f27
shubham88fru <shubham88fru@yahoo.com>
2024-05-11 10:31:52 -0700

    lowest common ancestor in bt

--- src/main/java/ptrn/trees/dfs/LowestCommonAncestorInBT.java
+++ src/main/java/ptrn/trees/dfs/LowestCommonAncestorInBT.java
@@ -6,6 +6,83 @@ import java.util.*;
 //@strvr - https://takeuforward.org/data-structure/lowest-common-ancestor-for-two-given-nodes/
 public class LowestCommonAncestorInBT {
 
+    //3) SWD's soln
+    /*****************************************************************************/
+    public TreeNode lowestCommonAncestorSWD(TreeNode root, TreeNode p, TreeNode q) {
+        return searchInSubTree(root, p, q);
+    }
+
+
+    private TreeNode searchInSubTree(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) return null;
+
+        if (root.val == p.val || root.val == q.val) return root;
+
+        TreeNode left = searchInSubTree(root.left, p, q);
+
+        TreeNode right = searchInSubTree(root.right, p, q);
+
+        //If left and right are non null means,
+        //the nodes are in separate (left and right) subtrees
+        //of the current root. And so, current root will be the
+        //lowest common ancestor for them.
+        if (left != null && right != null) return root;
+
+        //Note this works for this question, because
+        //this variation of the question guarantees that
+        //p and q certainly exist in the bt.
+        if (left != null) return left;
+        if (right != null) return right;
+
+        return null;
+    }
+    /*****************************************************************************/
+
+    //2) My sol 2 (kinda brute forceish)
+    /*****************************************************************************/
+    public TreeNode lowestCommonAncestor2(TreeNode root, TreeNode p, TreeNode q) {
+        List<TreeNode> ppath = new ArrayList<>();
+        List<TreeNode> qpath = new ArrayList<>();
+
+        //run two dfs for each nodes and get paths to each
+        //node in a list.
+        dfs(root, p, new ArrayList<>(), ppath);
+        dfs(root, q, new ArrayList<>(), qpath);
+
+        //take the paths and find the lowest common value.
+        return lowestCommon(ppath, qpath);
+    }
+
+    private void dfs(TreeNode root, TreeNode node, List<TreeNode> curr, List<TreeNode> path) {
+        if (root == null) return ;
+        curr.add(root);
+        if (root.val == node.val) {
+            for (TreeNode n: curr) {
+                path.add(n);
+            }
+        }
+
+
+        dfs(root.left, node, curr, path);
+        dfs(root.right, node, curr, path);
+        curr.remove(curr.size()-1);
+    }
+
+    private TreeNode lowestCommon(List<TreeNode> ppath, List<TreeNode> qpath) {
+        List<TreeNode> smaller = ppath.size() < qpath.size() ? ppath: qpath;
+        List<TreeNode> larger = ppath.size() >= qpath.size() ? ppath: qpath;
+
+        int s = smaller.size()-1;
+        while (s >= 0) {
+            if (larger.contains(smaller.get(s))) return smaller.get(s);
+            s -= 1;
+        }
+
+        return null;
+
+    }
+    /*****************************************************************************/
+
     //1) My sol1 (works but crappy)
     /*****************************************************************************/
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
@@ -75,79 +152,6 @@ public class LowestCommonAncestorInBT {
         }
 
         return map;
-    }
-    /*****************************************************************************/
-
-    //2) My sol 2 (kinda brute forceish)
-    /*****************************************************************************/
-    public TreeNode lowestCommonAncestor2(TreeNode root, TreeNode p, TreeNode q) {
-        List<TreeNode> ppath = new ArrayList<>();
-        List<TreeNode> qpath = new ArrayList<>();
-
-        //run two dfs for each nodes and get paths to each
-        //node in a list.
-        dfs(root, p, new ArrayList<>(), ppath);
-        dfs(root, q, new ArrayList<>(), qpath);
-
-        //take the paths and find the lowest common value.
-        return lowestCommon(ppath, qpath);
-    }
-
-    private void dfs(TreeNode root, TreeNode node, List<TreeNode> curr, List<TreeNode> path) {
-        if (root == null) return ;
-        curr.add(root);
-        if (root.val == node.val) {
-            for (TreeNode n: curr) {
-                path.add(n);
-            }
-        }
-
-
-        dfs(root.left, node, curr, path);
-        dfs(root.right, node, curr, path);
-        curr.remove(curr.size()-1);
-    }
-
-    private TreeNode lowestCommon(List<TreeNode> ppath, List<TreeNode> qpath) {
-        List<TreeNode> smaller = ppath.size() < qpath.size() ? ppath: qpath;
-        List<TreeNode> larger = ppath.size() >= qpath.size() ? ppath: qpath;
-
-        int s = smaller.size()-1;
-        while (s >= 0) {
-            if (larger.contains(smaller.get(s))) return smaller.get(s);
-            s -= 1;
-        }
-
-        return null;
-
-    }
-    /*****************************************************************************/
-
-    //3) SWD's soln
-    /*****************************************************************************/
-    public TreeNode lowestCommonAncestorSWD(TreeNode root, TreeNode p, TreeNode q) {
-        return searchInSubTree(root, p, q);
-    }
-
-
-    private TreeNode searchInSubTree(TreeNode root, TreeNode p, TreeNode q) {
-        if (root == null) return null;
-
-        if (root.val == p.val || root.val == q.val) return root;
-
-        TreeNode left = searchInSubTree(root.left, p, q);
-
-        TreeNode right = searchInSubTree(root.right, p, q);
-
-        if (left != null && right != null) return root;
-
-        //Note this works for this question, because
-        //this variation of the question guarantees that
-        //p and q certainly exist in the bt.
-        if (left != null) return left;
-        if (right != null) return right;
-
-        return null;
     }
     /*****************************************************************************/
 }

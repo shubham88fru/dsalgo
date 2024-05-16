3b18f5759dc433fc7ff80ba6bab6d124cdd6a3b5
shubham88fru <shubham88fru@yahoo.com>
2024-05-16 07:58:07 -0700

    zig zag level order traversal of BT

--- src/main/java/strvr/binarytree2/ZigZagTraversalOfBT.java
+++ src/main/java/strvr/binarytree2/ZigZagTraversalOfBT.java
@@ -4,11 +4,61 @@ import java.util.*;
 
 //@link - https://leetcode.com/problems/binary-tree-zigzag-level-order-traversal/description/
 //@strvr - https://takeuforward.org/data-structure/zig-zag-traversal-of-binary-tree/
+//@check - https://www.educative.io/module/page/Z4JLg2tDQPVv6QjgO/10370001/4976190424350720/6488203193483264
 public class ZigZagTraversalOfBT {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-        return traverseZigZag(root);
+        //return traverseZigZag(root);
+        return traverseZigZagOptimal(root);
     }
 
+    //1) Optimal - using a deque (not just as a simple queue).
+    //Alternate between the FIFO and LIFO behaviors at each level.
+    private List<List<Integer>> traverseZigZagOptimal(TreeNode root) {
+        List<List<Integer>> ans = new ArrayList<>();
+        if (root == null) return ans;
+
+        /**
+         will use the double ended feature of the deque.
+         */
+        Deque<TreeNode> q = new ArrayDeque<>();
+        q.addLast(root);
+        boolean reverseLevel = false;
+
+        while (!q.isEmpty()) {
+            int sz = q.size();
+            List<Integer> level = new ArrayList<>();
+            while (sz > 0) {
+                if (!reverseLevel) {
+                    //If reverse level isn't true,
+                    //we'll follow the normal BFS approach.
+                    //i.e. (Remove node - removeFirst()) -->
+                    //(add left - addLast()) --> (add right - addLast()).
+                    TreeNode node = q.removeFirst();
+                    level.add(node.val);
+                    if (node.left != null) q.addLast(node.left);
+                    if (node.right != null) q.addLast(node.right);
+
+                } else {
+                    //Otherwise, if reverse level is true,
+                    //we'll sort of reverse the normal BFS approach.
+                    //i.e. (Remove node - removeLast()) -->
+                    //(add right - addFirst()) --> (add left - addFirst()).
+                    TreeNode node = q.removeLast();
+                    level.add(node.val);
+                    if (node.right != null) q.addFirst(node.right);
+                    if (node.left != null) q.addFirst(node.left);
+                }
+
+                sz -= 1;
+            }
+            ans.add(level);
+            reverseLevel = !reverseLevel;
+        }
+
+        return ans;
+    }
+
+    //2) Suboptimal soln.
     private List<List<Integer>> traverseZigZag(TreeNode root) {
         List<List<Integer>> ans = new ArrayList<>();
         if (root == null) return ans;

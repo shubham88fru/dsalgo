19537d9b8cfd787a74714dbf37f46d9e5ac7cdb4
shubham88fru <shubham88fru@yahoo.com>
2024-05-31 08:37:01 -0700

    isomorphic strings

---
+++ src/main/java/ptrn/hashmap/IsomorphicStrings.java
@@ -0,0 +1,28 @@
+package ptrn.hashmap;
+
+import java.util.HashMap;
+import java.util.Map;
+
+//@link - https://leetcode.com/problems/isomorphic-strings/
+//@check - https://www.educative.io/module/page/Z4JLg2tDQPVv6QjgO/10370001/4976190424350720/4910548744667136
+public class IsomorphicStrings {
+    public boolean isIsomorphic(String s, String t) {
+        if (s.length() != t.length()) return false;
+
+        Map<Character, Character> mp = new HashMap<>();
+        Map<Character, Character> mp2 = new HashMap<>();
+        for (int i=0; i<s.length(); i++) {
+            char chs = s.charAt(i);
+            char cht = t.charAt(i);
+            if (!mp.containsKey(chs)) {
+                mp.put(chs, cht);
+            } else if (mp.get(chs) != cht) return false;
+
+            if (!mp2.containsKey(cht)) {
+                mp2.put(cht, chs);
+            } else if (mp2.get(cht) != chs) return false;
+        }
+
+        return true;
+    }
+}

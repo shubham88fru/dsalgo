a68cc72d65aa78f147a2a2bf58b599c4603c2e47
Shubham Singh <shubham88fru@yahoo.com>
2024-10-05 07:52:59 -0700

    sort list

--- src/main/java/swd/linkedlist/SortList.java
+++ src/main/java/swd/linkedlist/SortList.java
@@ -1,13 +1,115 @@
 package swd.linkedlist;
 
+import java.util.PriorityQueue;
+
 //@link - https://leetcode.com/problems/sort-list/description/
 public class SortList {
     public ListNode sortList(ListNode head) {
+        return revise(head);
+    }
+
+    //////////////////////////sol 1.
+    private ListNode revise(ListNode head) {
         return mergeSort(head);
     }
 
-    //Merge sort algorithm for linked list.
     private ListNode mergeSort(ListNode head) {
+        if (head == null) return null;
+        if (head.next == null) return head;
+
+
+        ListNode rightHalf = half(head);
+        ListNode leftHalf = head;
+
+        ListNode left = mergeSort(leftHalf);
+        ListNode right = mergeSort(rightHalf);
+        return mergeSortedLists(left, right);
+    }
+
+    private ListNode half(ListNode head) {
+        if (head == null) return null;
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        ListNode right = slow.next;
+        slow.next = null;
+        return right;
+    }
+
+    //Merge two sorted lists without using a third list.
+    private ListNode mergeSortedLists(ListNode a, ListNode b) {
+        ListNode currPA = a;
+        ListNode currPB = b;
+        while (currPA != null) {
+            currPA = currPA.next;
+        }
+
+
+        while (currPB != null) {
+            currPB = currPB.next;
+        }
+
+
+        ListNode dummy = new ListNode(-1);
+        dummy.next = b;
+        ListNode prev = dummy;
+        ListNode currA = a;
+        ListNode currB = b;
+
+        while (currA != null && currB != null) {
+            if (currA.val < currB.val) {
+                ListNode nextA = currA.next;
+                prev.next = currA;
+                currA.next = currB;
+                prev = currA;
+                currA = nextA;
+            } else {
+                prev.next = currB;
+                prev = currB;
+                ListNode nextB = currB.next;
+                currB.next = currA;
+                currB = nextB;
+            }
+        }
+        return dummy.next;
+    }
+    /////////////////////////sol1.
+
+    //////////////////////////sol2.
+    //uses extra space for the heap.
+    private ListNode revise2(ListNode head) {
+        PriorityQueue<ListNode> minHeap = new PriorityQueue<>((l1, l2) -> l1.val - l2.val);
+        ListNode dummy = new ListNode(-1);
+        ListNode curr = head;
+
+        while (curr != null) {
+            minHeap.add(curr);
+            ListNode prev = curr;
+            curr = curr.next;
+            prev.next = null;
+        }
+
+        curr = dummy;
+        while (!minHeap.isEmpty()) {
+            curr.next = minHeap.remove();
+            curr = curr.next;
+        }
+
+        return dummy.next;
+
+    }
+    //////////////////////////sol2.
+
+
+    ///////////////////////////sol3.
+    //Merge sort algorithm for linked list.
+    private ListNode mergeSort2(ListNode head) {
         //if head null or has just has one node, then it
         //is already sorted and we don't need to do anything.
         if (head == null || head.next == null) return head;
@@ -70,4 +172,5 @@ public class SortList {
 
         return dummy.next;
     }
+    //////////////////////////////sol3.
 }

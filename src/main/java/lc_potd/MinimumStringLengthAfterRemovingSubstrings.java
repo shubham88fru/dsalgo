fe7f5cf2688bd85502b45570bed295416c603a01
Shubham Singh <shubham88fru@yahoo.com>
2024-10-07 07:54:59 -0700

    minimum string length after removing substrings

--- src/main/java/lc_potd/MinimumStringLengthAfterRemovingSubstrings.java
+++ src/main/java/lc_potd/MinimumStringLengthAfterRemovingSubstrings.java
@@ -4,7 +4,13 @@ import java.util.ArrayDeque;
 import java.util.Deque;
 
 //@link - https://leetcode.com/problems/minimum-string-length-after-removing-substrings/
+//@check - https://www.youtube.com/watch?v=4XLzLdAE4Lc&ab_channel=codestorywithMIK
 public class MinimumStringLengthAfterRemovingSubstrings {
+
+    /*
+        My sol. TC: O(N), SC: O(N)
+        Mik showed an approach that is O(1) SC.
+     */
     public int minLength(String s) {
         Deque<Character> stack = new ArrayDeque<>();
 

74b0e4812ef3ec7a095934f6bc36bafe43842eff
Shubham Singh <shubham88fru@yahoo.com>
2024-10-18 07:24:12 -0700

    count number of maximum bitwise or subsets

---
+++ src/main/java/lc_potd/CountNumberOfMaximumBitwiseORSubsets.java
@@ -0,0 +1,66 @@
+package lc_potd;
+
+import java.util.*;
+
+//@link - https://leetcode.com/problems/count-number-of-maximum-bitwise-or-subsets/
+//@check - https://www.youtube.com/watch?v=DQOgVNI6BcE&t=1025s&ab_channel=codestorywithMIK
+public class CountNumberOfMaximumBitwiseORSubsets {
+    public int countMaxOrSubsets(int[] nums) {
+        //Max `or` in an array of integers will
+        //always be obtained by or'ing all the elements
+        //of the array.
+        int maxOr = 0;
+        for (int i=0; i<nums.length; i++) {
+            maxOr |= nums[i];
+        }
+
+        // backtracking.
+        // int[] cnt = {0};
+        // count(nums, 0, 0, cnt, new ArrayList<>(), new HashSet<>(), maxOr);
+        // return cnt[0];
+
+        //dp - memoization.
+        return countMemo(nums, 0, 0, maxOr, new HashMap<>());
+    }
+
+    //Dp - memoization soln. (Inspired by mik)
+    private int countMemo(int[] nums, int curr, int or, int maxOr, Map<String, Integer> memo) {
+        if (curr >= nums.length) {
+            if (or == maxOr) return 1;
+            return 0;
+        }
+
+
+        String key = curr + "_" + or;
+        if (memo.containsKey(key)) return memo.get(key);
+
+        int pick = countMemo(nums, curr+1, (or|nums[curr]), maxOr, memo);
+
+        int notPick = countMemo(nums, curr+1, or, maxOr, memo);
+
+        memo.put(key, pick + notPick);
+        return memo.get(key);
+    }
+
+    //Backtracking soln - my soln.
+    private void count(int[] nums, int curr, int or, int[] cnt, List<Integer> chosen, Set<String> cache, int maxOr) {
+        if (or == maxOr) {
+            List<Integer> lst = new ArrayList<>(chosen);
+            Collections.sort(lst);
+            String key = lst.toString();
+            if (!cache.contains(key)) {
+                cnt[0] += 1;
+                cache.add(key);
+            }
+        }
+        if (curr >= nums.length) return;
+
+
+        chosen.add(curr);
+        count(nums, curr+1, (or|nums[curr]), cnt, chosen, cache, maxOr);
+        chosen.remove(chosen.size()-1);
+
+
+        count(nums, curr+1, or, cnt, chosen, cache, maxOr);
+    }
+}

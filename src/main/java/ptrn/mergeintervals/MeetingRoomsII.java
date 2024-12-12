c8311faba701e8bbe541ad10e8256d10017f53c7
shubham88fru <shubham88fru@yahoo.com>
2024-12-12 07:18:57 -0700

    meeting rooms II

--- src/main/java/ptrn/mergeintervals/MeetingRoomsII.java
+++ src/main/java/ptrn/mergeintervals/MeetingRoomsII.java
@@ -1,10 +1,12 @@
 package ptrn.mergeintervals;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.PriorityQueue;
 
 //@link - https://www.codingninjas.com/studio/problems/meeting-room-ii_893289?leftPanelTabValue=PROBLEM
 //@check - https://www.educative.io/module/page/Z4JLg2tDQPVv6QjgO/10370001/4976190424350720/6375390508744704
+//         https://www.youtube.com/watch?v=FdzJmTCVyJU&t=512s&ab_channel=NeetCode
 //@tag - TO_REVISIT
 /***
  * Directly copied the algo from edctv.
@@ -12,6 +14,9 @@ import java.util.PriorityQueue;
 public class MeetingRoomsII {
     public static int findSets(int[][] intervals) {
 
+        /*
+        * Edctv's approach. Didn't understand need to revisit.
+        * */
         Arrays.sort(intervals, (a1, a2) -> a1[0] - a2[0]);
         PriorityQueue<Integer> pq = new PriorityQueue<>();
         pq.add(intervals[0][1]);
@@ -25,4 +30,42 @@ public class MeetingRoomsII {
 
         return pq.size();
     }
+
+    /*
+        Neetcode's approach. Didn't understand completely. Need to
+        revisit.
+     */
+    public int minMeetingRooms(List<Interval> intervals) {
+        int n = intervals.size();
+        int[] startTimes = new int[n];
+        int[] endTimes = new int[n];
+
+        for (int i=0; i<n; i++) {
+            startTimes[i] = intervals.get(i).start;
+            endTimes[i] = intervals.get(i).end;
+
+        }
+
+        Arrays.sort(startTimes);
+        Arrays.sort(endTimes);
+
+        int max = 0;
+        int i = 0;
+        int j = 0;
+        int count = 0;
+        while (i < n && j < n) {
+            if (startTimes[i] >= endTimes[j]) {
+                count -= 1;
+                j += 1;
+            } else {
+                count += 1;
+                i += 1;
+            }
+            max = Math.max(count, max);
+
+        }
+
+        return max;
+    }
+
 }

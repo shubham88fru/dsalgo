d0115e138a8b46fc3d43be6441dcf6809e1ae761
shubham88fru <shubham88fru@yahoo.com>
2024-12-21 08:48:27 -0700

    merge two sorted lists.

--- src/main/java/strvr/linkedlist1/MergeTwoSortedLists.java
+++ src/main/java/strvr/linkedlist1/MergeTwoSortedLists.java
@@ -9,6 +9,78 @@ public class MergeTwoSortedLists {
         return mergeTwoSortedListInPlace(list1, list2);
     }
 
+    //constant space
+    private ListNode revise1(ListNode list1, ListNode list2) {
+        if (list1 == null) return list2;
+        if (list2 == null) return list1;
+
+        //Make list1 as the base.
+        if (list2.val < list1.val) {
+            ListNode temp = list1;
+            list1 = list2;
+            list2 = temp;
+        }
+
+        ListNode curr1 = list1;
+        ListNode curr2 = list2;
+        ListNode prev = null;
+        ListNode dummy = new ListNode(-1);
+        dummy.next = list1;
+
+        while (curr1 != null && curr2 != null) {
+            if (curr1.val <= curr2.val) {
+                prev = curr1;
+                curr1 = curr1.next;
+            } else {
+                prev.next = curr2;
+                prev = curr2;
+                curr2 = curr2.next;
+                prev.next = curr1;
+            }
+        }
+
+        if (curr1 != null) prev.next = curr1;
+        else if (curr2 != null) prev.next = curr2;
+
+        return dummy.next;
+    }
+
+    //constant space.
+    private ListNode revise2(ListNode list1, ListNode list2) {
+        if (list1 == null) return list2;
+        if (list2 == null) return list1;
+
+        //Make list1 as the base.
+        if (list2.val < list1.val) {
+            ListNode temp = list1;
+            list1 = list2;
+            list2 = temp;
+        }
+
+        ListNode curr1 = list1;
+        ListNode curr2 = list2;
+        ListNode prev = new ListNode(-1);
+
+        while (curr1 != null && curr2 != null) {
+            if (curr1.val <= curr2.val) {
+                prev.next = curr1;
+                prev = curr1;
+                ListNode nxt = curr1.next;
+                curr1.next = curr2;
+                curr1 = nxt;
+            } else {
+
+                prev.next = curr2;
+                prev = curr2;
+                ListNode nxt = curr2.next;
+                curr2.next = curr1;
+                curr2 = nxt;
+            }
+        }
+
+        return list1;
+    }
+
     //1) Optimal approach - constant space solution.
     //T: O(n1+n2), S: O(1)
     private ListNode mergeTwoSortedListInPlace(ListNode l1, ListNode l2) {

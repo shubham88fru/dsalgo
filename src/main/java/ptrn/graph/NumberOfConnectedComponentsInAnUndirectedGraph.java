37bbd7f04a1d110ec9c52f1e48f73d9faf85721b
shubham88fru <shubham88fru@yahoo.com>
2025-02-26 13:36:04 -0700

    add comment

--- src/main/java/ptrn/graph/NumberOfConnectedComponentsInAnUndirectedGraph.java
+++ src/main/java/ptrn/graph/NumberOfConnectedComponentsInAnUndirectedGraph.java
@@ -6,6 +6,13 @@ import java.util.List;
 //@link - https://neetcode.io/problems/count-connected-components
 //@check - https://www.educative.io/module/page/Z4JLg2tDQPVv6QjgO/10370001/4976190424350720/5296217631490048
 public class NumberOfConnectedComponentsInAnUndirectedGraph {
+    /*
+    * This was a challenge problem in edctv and so no model soln
+    * was provided. Below is my soln but I don't think this is the
+    * optimal soln. This problem can be solved using Union find pattern
+    * and that might have a better TC.
+    * Check for optimal soln on YT.
+    * */
     public int countComponents(int n, int[][] edges) {
         List<List<Integer>> graph = getGraph(edges, n);
 

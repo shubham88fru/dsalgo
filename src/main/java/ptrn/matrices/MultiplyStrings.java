c8082865bc6b277b763093e10022df6fde4e2406
shubham88fru <shubham88fru@yahoo.com>
2025-03-01 16:08:27 -0700

    multiply strings

--- src/main/java/ptrn/matrices/MultiplyStrings.java
+++ src/main/java/ptrn/matrices/MultiplyStrings.java
@@ -5,12 +5,62 @@ import java.util.List;
 
 //@link - https://leetcode.com/problems/multiply-strings/
 //@check - https://www.educative.io/module/page/Z4JLg2tDQPVv6QjgO/10370001/4976190424350720/5816048567451648
+//       - https://www.youtube.com/watch?v=1vZswirL8Y8&t=638s&ab_channel=NeetCode
 public class MultiplyStrings {
+
     /*
-        My soln. There must be a better way to solve this question.
+    * 1. Nc soln. Coded by me based on nc's explanation. But may need to
+    * revise the soln again coz very easy to mess up.
+    * */
+    public String multiply(String num1, String num2) {
+        return optimal(num1, num2);
+    }
+
+    /*
+        Based on nc.
+    */
+    private String optimal(String num1, String num2) {
+        if ("0".equals(num1) || "0".equals(num2)) return "0";
+        int n1 = num1.length();
+        int n2 = num2.length();
+
+        StringBuffer sb1 = new StringBuffer(num1);
+        StringBuffer sb2 = new StringBuffer(num2);
+
+        sb1.reverse();
+        sb2.reverse();
+
+        int[] ans = new int[n1+n2];
+        for (int i=0; i<n1; i++) {
+            int d1 = sb1.charAt(i)-'0';
+            for (int j=0; j<n2; j++) {
+                int d2 = sb2.charAt(j)-'0';
+                int pdt = d1*d2;
+                ans[i+j+1] += (ans[i+j] + (pdt%10))/10 + (pdt/10);
+                ans[i+j] = (ans[i+j] + (pdt%10))%10;
+            }
+        }
+
+        StringBuffer res = new StringBuffer();
+        for (int i=0; i<ans.length; i++) {
+            res.append(ans[i]);
+        }
+
+        res.reverse();
+        int i = 0;
+        for (; i<res.length(); i++) {
+            if (res.charAt(i) != '0') break;
+        }
+
+        return res.substring(i).toString();
+    }
+
+
+    /*
+        2. My soln. There must be a better way to solve this question.
         Check mik/yt on next encounter.
     */
-    public String multiply(String num1, String num2) {
+    public String multiply2(String num1, String num2) {
         if (num1.equals("0") || num2.equals("0")) return "0";
 
         if (num1.length() < num2.length()) {

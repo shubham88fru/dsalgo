75af8be21f71a8ca94abb844e174eb9e76fa82f8
shubham88fru <shubham88fru@yahoo.com>
2025-03-02 16:38:38 -0700

    sum of two integers

--- src/main/java/ptrn/bitmanipulation/SumOfTwoIntegers.java
+++ src/main/java/ptrn/bitmanipulation/SumOfTwoIntegers.java
@@ -2,6 +2,7 @@ package ptrn.bitmanipulation;
 
 //@link - https://leetcode.com/problems/sum-of-two-integers/
 //@check - https://www.educative.io/module/page/Z4JLg2tDQPVv6QjgO/10370001/4976190424350720/6491145648734208
+//       - https://www.youtube.com/watch?v=gVUrDV4tZfY&t=333s&ab_channel=NeetCode
 public class SumOfTwoIntegers {
     public int getSum(int a, int b) {
         // return lol(a, b);
@@ -13,12 +14,14 @@ public class SumOfTwoIntegers {
         No clue why this works. If this is a
         recurring problem for some company, check YT
         for explanation.
+
+        UPDATE: checked nc's explanation. Attached link.
     */
     private int optimal(int a, int b) {
         while (b != 0) {
-            int carry = a & b;
-            a = a ^ b;
-            b = carry << 1;
+            int carry = a & b; //this will give the carry
+            a = a ^ b; //gives the right bit as we should get when performing binary addition.
+            b = carry << 1; //but carry will go the next digits, hence we need to shift
         }
 
         return a;

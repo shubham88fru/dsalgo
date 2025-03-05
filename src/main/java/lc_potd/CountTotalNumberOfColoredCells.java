3d4f67e507f6c166dee0eca36e661f9ec0a01705
shubham88fru <shubham88fru@yahoo.com>
2025-03-05 06:04:06 -0700

    count total number of colored cells

---
+++ src/main/java/lc_potd/CountTotalNumberOfColoredCells.java
@@ -0,0 +1,34 @@
+package lc_potd;
+
+//@link - https://leetcode.com/problems/count-total-number-of-colored-cells/
+//@check - https://www.youtube.com/watch?v=LakDs5OBiGM&t=352s&ab_channel=codestorywithMIK
+public class CountTotalNumberOfColoredCells {
+    public long coloredCells(int n) {
+        return pass1(n);
+    }
+
+    /*
+        Had to take a slight hint from mik. But mik showed a
+        different math soln.
+        My first intuition was to solve it using
+        BFS, I think BFS can be done for this problem.
+
+        This soln is completely based on observation.
+     */
+    private long pass1(int n) {
+
+        //By observation, for each given n, the
+        //number of cells at the middle layer will be n + (n-1)
+        //and then 1-1 layer of decrementing number of cells on each side.
+        long middleLayer = n + (n-1);
+
+        long cells = middleLayer;
+        n = (int)middleLayer-2;
+        while (n > 0) {
+            cells += 2*n; //*2 because these layers will be on each side.
+            n = n-2;
+        }
+
+        return cells;
+    }
+}

a7cc634f008f536aaabd3209cf57bab469ee0208
shubham88fru <shubham88fru@yahoo.com>
2025-03-07 07:37:26 -0700

    target sum

--- src/main/java/swd/dp/TargetSum.java
+++ src/main/java/swd/dp/TargetSum.java
@@ -7,6 +7,27 @@ public class TargetSum {
         return waysForTargetSum(nums, target, 0);
     }
 
+    //1) With memoization.
+    private int revise(int[] nums, int target) {
+        int n = nums.length;
+
+        Integer[][] dp = new Integer[21][3000];
+        return dp(nums, target, 0, dp);
+    }
+
+    private int dp(int[] nums, int target, int i, Integer[][] dp) {
+        if (i >= nums.length && target == 0) return 1;
+        if (i >= nums.length) return 0;
+
+        if (dp[i][target+1000] != null) return dp[i][target+1000];
+        int plus = dp(nums, target-nums[i], i+1, dp);
+        int minus = dp(nums, target+nums[i], i+1, dp);
+
+        dp[i][target+1000] = plus + minus;
+        return dp[i][target+1000];
+    }
+
+    //2) Without memoization
     private int waysForTargetSum(int[] nums, int target, int currIndex) {
 
         //target sum should be acheived only after considering the last element.

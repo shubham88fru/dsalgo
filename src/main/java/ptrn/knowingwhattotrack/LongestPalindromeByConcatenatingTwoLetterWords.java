4b66faf50e17c3fb84171812d0ae85676b4d7936
shubham88fru <shubham88fru@yahoo.com>
2025-05-25 06:19:09 -0700

    longest palindrome by concatenating two letter words

--- src/main/java/ptrn/knowingwhattotrack/LongestPalindromeByConcatenatingTwoLetterWords.java
+++ src/main/java/ptrn/knowingwhattotrack/LongestPalindromeByConcatenatingTwoLetterWords.java
@@ -1,13 +1,51 @@
 package ptrn.knowingwhattotrack;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 //@link - https://leetcode.com/problems/longest-palindrome-by-concatenating-two-letter-words/description/
 //@check - https://www.educative.io/module/page/Z4JLg2tDQPVv6QjgO/10370001/4976190424350720/4996689485889536
 public class LongestPalindromeByConcatenatingTwoLetterWords {
-    //1) My soln.
+
     public int longestPalindrome(String[] words) {
+        return revise(words);
+//        return mysol1(words);
+//        return edctvSol(words);
+    }
+
+    //1) my soln. mik also had the soln on similar lines.
+    private int revise(String[] words) {
+        int n = words.length;
+
+        int sameMax = Integer.MIN_VALUE;
+        Map<String, Integer> st = new HashMap<>();
+        Set<String> same = new HashSet<>();
+
+        int len = 0;
+        for (String word: words) {
+            String rev = (new StringBuffer(word)).reverse().toString();
+            if (word.equals(rev)) {
+                if (same.contains(word)) {
+                    len += 4;
+                    same.remove(word);
+                } else same.add(word);
+            } else if (st.containsKey(rev)) {
+                len += 4;
+                st.put(rev, st.get(rev)-1);
+                if (st.get(rev) == 0) st.remove(rev);
+            } else {
+                st.put(word, st.getOrDefault(word, 0)+1);
+            }
+        }
+
+        if (!same.isEmpty()) len += 2;
+        return len;
+    }
+
+    //2) My soln - 1 is better coz only one loop.
+    private int mysol1(String[] words) {
         Map<String, Integer> mp = new HashMap<>();
         int len = 0;
         for (String word: words) {
@@ -40,20 +78,8 @@ public class LongestPalindromeByConcatenatingTwoLetterWords {
         return len;
     }
 
-    private boolean palindrome(String s) {
-        int l = 0;
-        int r = s.length() - 1;
-        while (l <= r) {
-            if (s.charAt(l) != s.charAt(r)) return false;
-            l += 1;
-            r -= 1;
-        }
-
-        return true;
-    }
-
     //2) Edctv soln also similar with slight differences.
-    public static int longestPalindrome2(String[] words) {
+    public static int edctvSol(String[] words) {
         // Create a HashMap to store word frequencies
         HashMap<String, Integer> frequencies = new HashMap<>();
 
@@ -95,5 +121,17 @@ public class LongestPalindromeByConcatenatingTwoLetterWords {
         }
 
         return 2 * count;
+    }
+
+    private boolean palindrome(String s) {
+        int l = 0;
+        int r = s.length() - 1;
+        while (l <= r) {
+            if (s.charAt(l) != s.charAt(r)) return false;
+            l += 1;
+            r -= 1;
+        }
+
+        return true;
     }
 }

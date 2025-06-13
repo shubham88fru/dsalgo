f586f7cf64763b88cc8d88bf78a940c14fd06106
shubham88fru <shubham88fru@yahoo.com>
2025-06-13 13:34:48 -0500

    add notes

--- src/main/java/lc_potd/MaximumDifferenceBetweenAdjacentElementsInACircularArray.java
+++ src/main/java/lc_potd/MaximumDifferenceBetweenAdjacentElementsInACircularArray.java
@@ -3,6 +3,13 @@ package lc_potd;
 //@link - https://leetcode.com/problems/maximum-difference-between-adjacent-elements-in-a-circular-array/
 public class MaximumDifferenceBetweenAdjacentElementsInACircularArray {
     public int maxAdjacentDistance(int[] nums) {
+        /*
+        * can use this property
+        * of circular array (for a more
+        * cleaner soln)
+        * nums[i+1] = nums[(i+1)%n]
+        * @see miks soln.
+        * */
         int maxDiff = Integer.MIN_VALUE;
         int n = nums.length;
         for (int i=1; i<n; i++) {
@@ -12,5 +19,6 @@ public class MaximumDifferenceBetweenAdjacentElementsInACircularArray {
         maxDiff = Math.max(maxDiff, Math.abs(nums[n-1]-nums[0]));
 
         return maxDiff;
+
     }
 }

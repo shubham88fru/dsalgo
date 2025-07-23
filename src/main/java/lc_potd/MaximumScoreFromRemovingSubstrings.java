66cc688f2e6160d6ac88a577db1fb4de09830ea9
shubham88fru <shubham88fru@yahoo.com>
2025-07-23 07:59:54 -0500

    maximum score from removing substrings

--- src/main/java/lc_potd/MaximumScoreFromRemovingSubstrings.java
+++ src/main/java/lc_potd/MaximumScoreFromRemovingSubstrings.java
@@ -53,5 +53,52 @@ public class MaximumScoreFromRemovingSubstrings {
         return points;
 
     }
+
+    /// ////////////
+    /*
+        My soln. Pretty much similar to aryan's
+     */
+    public int maximumGain2(String s, int x, int y) {
+        return revise(s, x, y);
+    }
+
+    private int revise(String s, int x, int y) {
+        if (x >= y) {
+            return delete(s, 'a', 'b', x, y);
+        }
+
+        return delete(s, 'b', 'a', y, x);
+    }
+
+    private int delete(String str, char f, char s, int x, int y) {
+        int n = str.length();
+        Deque<Character> stack = new ArrayDeque<>();
+        int score = 0;
+        for (int i=0; i<n; i++) {
+            char ch = str.charAt(i);
+            if (stack.isEmpty() || stack.peekFirst() != f || ch != s) stack.addFirst(ch);
+            else if (stack.peekFirst() == f && ch == s) {
+                stack.removeFirst();
+                score += x;
+            }
+        }
+
+
+        StringBuffer sb = new StringBuffer();
+        while (!stack.isEmpty()) sb.append(stack.removeFirst());
+
+        n = sb.length();
+        for (int i=0; i<n; i++) {
+            char ch = sb.charAt(i);
+            if (stack.isEmpty() || stack.peekFirst() != f || ch != s) stack.addFirst(ch);
+            else if (stack.peekFirst() == f && ch == s) {
+                stack.removeFirst();
+                score += y;
+            }
+        }
+
+        return score;
+
+    }
 }
 

10ea256447cd9d9e962b323cc5e1d56b3e0394a8
shubham88fru <shubham88fru@yahoo.com>
2025-08-29 16:10:29 -0500

    alice and bob playing flower game

---
+++ src/main/java/lc_potd/AliceAndBobPlayingFlowerGame.java
@@ -0,0 +1,27 @@
+package lc_potd;
+
+//@link - https://leetcode.com/problems/alice-and-bob-playing-flower-game/
+public class AliceAndBobPlayingFlowerGame {
+    public long flowerGame(int n, int m) {
+        return pass1(n, m);
+    }
+
+    private long pass1(int n, int m) {
+        long e1 = 0;
+        long o1 = 0;
+        long e2 = 0;
+        long o2 = 0;
+
+        for (int i=1; i<=n; i++) {
+            if (i%2==0) e1 += 1;
+            else o1 += 1;
+        }
+
+        for (int i=1; i<=m; i++) {
+            if (i%2==0) e2 += 1;
+            else o2 += 1;
+        }
+
+        return (long)(e1*o2) + (long)(o1*e2);
+    }
+}

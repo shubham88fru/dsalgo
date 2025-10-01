2d8183ad65d8e812841b02439c6e9f5e8ce3b7df
shubham88fru <shubham88fru@yahoo.com>
2025-10-01 07:38:30 -0500

    water bottles

--- src/main/java/lc_potd/WaterBottles.java
+++ src/main/java/lc_potd/WaterBottles.java
@@ -3,6 +3,30 @@ package lc_potd;
 //@link - https://leetcode.com/problems/water-bottles/
 public class WaterBottles {
     public int numWaterBottles(int numBottles, int numExchange) {
+        return revise(numBottles, numExchange);
+    }
+
+    /**
+     My soln.
+     Main intuition is that each time
+     drink 'all' the full bottles.
+     */
+    private int revise(int numBottles, int numExchange) {
+
+        int count = 0;
+        int f = numBottles; //full bottles
+        int e = 0; //empty bottles
+        while (f != 0) {
+            count += f;
+            e += f;
+            f = e/numExchange;
+            e = e%numExchange;
+        }
+
+        return count;
+    }
+
+    private int approach2(int numBottles, int numExchange) {
         int fullBottles = numBottles;
         int emptyBottles = 0;
         boolean drink = true;

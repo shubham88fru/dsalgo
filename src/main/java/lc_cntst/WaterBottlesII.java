f59bfa5c06e5b7c2f85b69ee1f816478689586d8
shubham88fru <shubham88fru@yahoo.com>
2025-10-02 08:36:58 -0500

    water bottles II

--- src/main/java/lc_cntst/WaterBottlesII.java
+++ src/main/java/lc_cntst/WaterBottlesII.java
@@ -3,6 +3,10 @@ package lc_cntst;
 //@link - https://leetcode.com/problems/water-bottles-ii/
 public class WaterBottlesII {
     public int maxBottlesDrunk(int numBottles, int numExchange) {
+        return revise(numBottles, numExchange);
+    }
+
+    private int approach2(int numBottles, int numExchange) {
         int drunk = 0;
         int empty = 0;
         while (numBottles > 0 || empty >= numExchange) {
@@ -19,5 +23,26 @@ public class WaterBottlesII {
         }
 
         return drunk;
+    }
+
+    private int revise(int numBottles, int numExchange) {
+
+        int f = numBottles;
+        int e = 0;
+
+        int count = 0;
+        while ((f > 0) || (e >= numExchange)) {
+            if (e >= numExchange) {
+                e -= numExchange;
+                f += 1;
+                numExchange += 1;
+            } else {
+                count += f;
+                e += f;
+                f = 0;
+            }
+        }
+
+        return count;
     }
 }

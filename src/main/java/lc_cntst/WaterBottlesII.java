ba994b492e3e85a3cab92206f8db1852ddad6e47
shubham88fru <shubham88fru@yahoo.com>
2025-10-03 08:18:35 -0500

    refactor

--- src/main/java/lc_cntst/WaterBottlesII.java
+++ src/main/java/lc_cntst/WaterBottlesII.java
@@ -6,25 +6,6 @@ public class WaterBottlesII {
         return revise(numBottles, numExchange);
     }
 
-    private int approach2(int numBottles, int numExchange) {
-        int drunk = 0;
-        int empty = 0;
-        while (numBottles > 0 || empty >= numExchange) {
-            if (empty >= numExchange) {
-                empty -= numExchange;
-                numExchange += 1;
-                numBottles += 1;
-            } else if (numBottles > 0) {
-                drunk += numBottles;
-                empty += numBottles;
-                numBottles = 0;
-
-            }
-        }
-
-        return drunk;
-    }
-
     private int revise(int numBottles, int numExchange) {
 
         int f = numBottles;
@@ -45,4 +26,25 @@ public class WaterBottlesII {
 
         return count;
     }
+
+    private int approach2(int numBottles, int numExchange) {
+        int drunk = 0;
+        int empty = 0;
+        while (numBottles > 0 || empty >= numExchange) {
+            if (empty >= numExchange) {
+                empty -= numExchange;
+                numExchange += 1;
+                numBottles += 1;
+            } else if (numBottles > 0) {
+                drunk += numBottles;
+                empty += numBottles;
+                numBottles = 0;
+
+            }
+        }
+
+        return drunk;
+    }
+
+
 }

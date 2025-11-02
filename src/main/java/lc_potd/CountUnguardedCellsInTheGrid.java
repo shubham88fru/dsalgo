41d8b66eba749553f3f16260b9223c86b4fbd700
shubham88fru <shubham88fru@yahoo.com>
2025-11-02 11:12:03 -0600

    count unguarded cells in the grid

--- src/main/java/lc_potd/CountUnguardedCellsInTheGrid.java
+++ src/main/java/lc_potd/CountUnguardedCellsInTheGrid.java
@@ -15,8 +15,6 @@ public class CountUnguardedCellsInTheGrid {
             matrix[walls[i][0]][walls[i][1]] = -2;
         }
 
-        // printMatrix(matrix);
-
         for (int i=0; i<m; i++) {
             for (int j=0; j<n; j++) {
                 if (matrix[i][j] == -1) {
@@ -62,10 +60,72 @@ public class CountUnguardedCellsInTheGrid {
         }
     }
 
+    //revise
+    private int revise(int m, int n, int[][] guards, int[][] walls) {
+        int[][] mat = new int[m][n];
 
-    private void printMatrix(int[][] matrix) {
-        for (int i=0; i<matrix.length; i++) {
-            System.out.println(Arrays.toString(matrix[i]));
+        for (int[] guard: guards) {
+            mat[guard[0]][guard[1]] = 1;
+        }
+
+        for (int[] wall: walls) {
+            mat[wall[0]][wall[1]] = 2;
+        }
+
+        for (int[] guard: guards) {
+            markLeft(mat, guard[0], guard[1]-1);
+            markUp(mat, guard[0]-1, guard[1]);
+            markRight(mat, guard[0], guard[1]+1);
+            markDown(mat, guard[0]+1, guard[1]);
+        }
+
+        int count = 0;
+        for (int i=0; i<m; i++) {
+            for (int j=0; j<n; j++) {
+                if (mat[i][j] == 0) count += 1;
+            }
+        }
+
+        return count;
+    }
+
+    private void markLeft(int[][] mat, int i, int j) {
+        int m = mat.length;
+        int n = mat[0].length;
+
+        for (int col=j; col >=0; col--) {
+            if (mat[i][col] == 2 || mat[i][col] == 1) break;
+            mat[i][col] = -1;
+        }
+    }
+
+    private void markUp(int[][] mat, int i, int j) {
+        int m = mat.length;
+        int n = mat[0].length;
+
+        for (int row=i; row >=0; row--) {
+            if (mat[row][j] == 2 || mat[row][j] == 1) break;
+            mat[row][j] = -1;
+        }
+    }
+
+    private void markRight(int[][] mat, int i, int j) {
+        int m = mat.length;
+        int n = mat[0].length;
+
+        for (int col=j; col < n; col++) {
+            if (mat[i][col] == 2 || mat[i][col] == 1) break;
+            mat[i][col] = -1;
+        }
+    }
+
+    private void markDown(int[][] mat, int i, int j) {
+        int m = mat.length;
+        int n = mat[0].length;
+
+        for (int row=i; row < m; row++) {
+            if (mat[row][j] == 2 || mat[row][j] == 1) break;
+            mat[row][j] = -1;
         }
     }
 }

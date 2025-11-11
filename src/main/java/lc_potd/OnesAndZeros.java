9e91e46fd17436808591b2b9972b03cd41147c14
shubham88fru <shubham88fru@yahoo.com>
2025-11-11 08:04:35 -0600

    ones and zeros

---
+++ src/main/java/lc_potd/OnesAndZeros.java
@@ -0,0 +1,46 @@
+package lc_potd;
+
+//@link - https://leetcode.com/problems/ones-and-zeroes/description/?
+//@check - https://www.youtube.com/watch?v=P_mC0wOTNV4
+public class OnesAndZeros {
+    public int findMaxForm(String[] strs, int m, int n) {
+        return pass1(strs, m, n);
+    }
+
+    private int pass1(String[] strs, int m, int n) {
+        Integer[][][] dp = new Integer[strs.length+1][m+1][n+1];
+        return dp(strs, dp, 0, m, n);
+    }
+
+    private int dp(String[] strs, Integer[][][] dp, int i, int m, int n) {
+        if (i >= strs.length) return 0;
+        if (m < 0 || n < 0) return 0;
+
+        if (dp[i][m][n] != null) return dp[i][m][n];
+
+        int[] cnt = count(strs[i]);
+
+        int pick = 0;
+        if (cnt[0] <= m && cnt[1] <= n) {
+            pick = 1 + dp(strs, dp, i+1, m-cnt[0], n-cnt[1]);
+        }
+
+        int nPick = dp(strs, dp, i+1, m, n);
+
+        dp[i][m][n] = Math.max(pick, nPick);
+
+        return dp[i][m][n];
+    }
+
+    private int[] count(String str) {
+        int o = 0;
+        int z = 0;
+        for (int i=0; i<str.length(); i++) {
+            char ch = str.charAt(i);
+            if (ch == '0') o += 1;
+            else z += 1;
+        }
+
+        return new int[] {o, z};
+    }
+}

e726961a0ee5e770a873b405efcce32cbbcdfc5b
shubham88fru <shubham88fru@yahoo.com>
2025-11-16 09:28:52 -0600

    number of susbtrings with only 1s

--- src/main/java/lc_potd/NumberOfSubstringsWithOnly1s.java
+++ src/main/java/lc_potd/NumberOfSubstringsWithOnly1s.java
@@ -3,7 +3,35 @@ package lc_potd;
 //@link - https://leetcode.com/problems/number-of-substrings-with-only-1s/description/
 public class NumberOfSubstringsWithOnly1s {
     public int numSub(String s) {
-        return optimal(s);
+        // return optimal(s);
+        return optimal2(s);
+    }
+
+    /**
+     This is even better than
+     my other soln. Don't have to
+     deal with multiplications etc.
+     */
+    private int optimal2(String s) {
+        int n = s.length();
+
+        int l = 0;
+        int r = 0;
+
+        long ans = 0;
+        final int MOD = 1000000007;
+        while (r < n) {
+
+            while (r < n && s.charAt(r) == '1') {
+                r += 1;
+                ans += (r-l); //sum of natural nums
+            }
+
+            r += 1;
+            l = r;
+        }
+
+        return (int)(ans%MOD);
     }
 
     /**
@@ -29,7 +57,7 @@ public class NumberOfSubstringsWithOnly1s {
 
             if (s.charAt(l) == '1') {
                 long m = r-l;
-                long sum = m*(m+1)/2;
+                long sum = m*(m+1)/2; //sum of natural nums.
                 ans = (ans + sum);
             }
 

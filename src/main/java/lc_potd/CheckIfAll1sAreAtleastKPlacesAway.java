896eeaef98c0d2dc081e2b6bf86878b12d0eb9b5
shubham88fru <shubham88fru@yahoo.com>
2025-11-17 09:09:39 -0600

    check if all 1s are atleast k places away

---
+++ src/main/java/lc_potd/CheckIfAll1sAreAtleastKPlacesAway.java
@@ -0,0 +1,50 @@
+package lc_potd;
+
+//@link - https://leetcode.com/problems/check-if-all-1s-are-at-least-length-k-places-away/description/?
+public class CheckIfAll1sAreAtleastKPlacesAway {
+    public boolean kLengthApart(int[] nums, int k) {
+        // return revise(nums, k);
+        return optimal(nums, k);
+    }
+
+    /**
+     Better.
+     */
+    private boolean optimal(int[] nums, int k) {
+        int n = nums.length;
+
+        int pi = -1;
+        for (int i=0; i<n; i++) {
+            if (nums[i] == 1) {
+                if (pi != -1 && i-pi-1 < k) return false;
+                pi = i;
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     My initial intuition.
+     But this is dumb, tbh.
+     Why did I decide to keep track of
+     the dist and check at the end? :/
+     We can return early.
+     */
+    private boolean revise(int[] nums, int k) {
+        int n = nums.length;
+
+        int pi = -1;
+        int dist = Integer.MAX_VALUE;
+        for (int i=0; i<n; i++) {
+            if (nums[i] == 1) {
+                if (pi != -1) {
+                    dist = Math.min(i-pi-1, dist);
+                }
+                pi = i;
+            }
+        }
+
+        return dist >= k;
+    }
+}

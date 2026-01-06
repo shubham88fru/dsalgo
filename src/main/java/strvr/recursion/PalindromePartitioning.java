1b85ccba2ba243eece95874b0e41d19afffb70ec
shubham88fru <shubham88fru@yahoo.com>
2026-01-06 06:46:38 -0600

    palindrome partitioning

--- src/main/java/strvr/recursion/PalindromePartitioning.java
+++ src/main/java/strvr/recursion/PalindromePartitioning.java
@@ -7,47 +7,91 @@ import java.util.List;
 //@strvr - https://takeuforward.org/data-structure/palindrome-partitioning/
 public class PalindromePartitioning {
     public List<List<String>> partition(String s) {
-        //will store all result here.
-        List<List<String>> result = new ArrayList<>();
-        generateAllCombinations(s, 0, s.length()-1, new ArrayList<>(), result);
-
-        //return the populated result.
-        return result;
+        // return revise(s);
+        return approach2(s);
     }
 
-    private void generateAllCombinations(String s, int start, int end, ArrayList<String> currentConfiguration, List<List<String>> result) {
+    private List<List<String>> approach2(String s) {
+        List<List<String>> ans = new ArrayList<>();
+        int n = s.length();
 
-        //base case. no more substrings possible.
-        if (start > end) {
-            result.add(new ArrayList<>(currentConfiguration));
+        //Pre-fill the palindrome array.
+        //for any substring i..j, this array
+        //will tell if that substring is a palindrome
+        //in O(1)
+        boolean[][] palindrome = new boolean[n][n];
+        for (int i=0; i<n; i++) {
+            //all single length
+            //substrings are palindrome.
+            palindrome[i][i] = true;
+        }
+
+        for (int L=2; L<=n; L++) { //for each possible length
+            for (int i=0; i<n-L+1; i++) { //check from each index
+                int j = i+L-1; //end index.
+                if (s.charAt(i) == s.charAt(j)) {
+                    if (L == 2) {
+                        palindrome[i][j] = true;
+                    } else {
+                        palindrome[i][j] = palindrome[i+1][j-1]; //ans will depend on inner string.
+                    }
+                }
+            }
+        }
+
+        backtrack2(s, 0, palindrome, ans, new ArrayList<>());
+        return ans;
+    }
+
+    private void backtrack2(String s, int i, boolean[][] palindrome, List<List<String>> ans, List<String> curr) {
+        if (i >= s.length()) {
+            ans.add(new ArrayList<>(curr));
             return;
         }
 
-        //for each substring, we'll try to make a
-        //cut (new substring) at each index. That too,
-        //only if left substring is a valid palindrome.
-        for (int i=start; i <= end; i++) {
-            String sub = s.substring(start, i+1);
-            if (isPalindrome(sub)) {
-                //if left part is a palindrome, add it to
-                //list and recursively call for right part (sub string)
-                currentConfiguration.add(sub);
-                generateAllCombinations(s, i+1, end, currentConfiguration, result);
-
-                //once done, backtrack to reverset the change done to `currentConfiguration`
-                //to original state.
-                currentConfiguration.remove(currentConfiguration.size()-1);
+        for (int j=i; j<s.length(); j++) {
+            String leftSub = s.substring(i, j+1);
+            if (palindrome[i][j]) {
+                curr.add(leftSub);
+                backtrack2(s, j+1, palindrome, ans, curr);
+                curr.remove(curr.size()-1);
             }
         }
     }
 
+    private List<List<String>> revise(String s) {
+        List<List<String>> ans = new ArrayList<>();
+
+        backtrack(s, ans, new ArrayList<>());
+        return ans;
+    }
+
+    private void backtrack(String s, List<List<String>> ans, List<String> curr) {
+        if (s.length() == 0) {
+            ans.add(new ArrayList<>(curr));
+            return;
+        }
+
+        for (int i=0; i<s.length(); i++) {
+            String leftSub = s.substring(0, i+1);
+            if (isPalindrome(leftSub)) {
+                curr.add(leftSub);
+                backtrack(s.substring(i+1), ans, curr);
+                curr.remove(curr.size()-1);
+            }
+        }
+    }
+
+
+
     private boolean isPalindrome(String s) {
-        int start = 0;
-        int end = s.length()-1;
-        while (start <= end) {
-            if (s.charAt(start) != s.charAt(end)) return false;
-            start++;
-            end--;
+        int l = 0;
+        int r = s.length()-1;
+
+        while (l <= r) {
+            if (s.charAt(l) != s.charAt(r)) return false;
+            l += 1;
+            r -= 1;
         }
 
         return true;

f9e30912782d7abbcdb1c7fd45b303c2b84b80ca
shubham88fru <shubham88fru@yahoo.com>
2026-01-18 09:14:40 -0600

    longest palindromic substring

--- src/main/java/ptrn/dp/LongestPalindromicSubstring.java
+++ src/main/java/ptrn/dp/LongestPalindromicSubstring.java
@@ -6,9 +6,36 @@ package ptrn.dp;
 public class LongestPalindromicSubstring {
     public String longestPalindrome(String s) {
         // return brute(s);
-        return recursionWithMemoization(s);
+        // return recursionWithMemoization(s);
+        return bottomUpDp(s);
     }
 
+    private String bottomUpDp(String s) {
+        int n = s.length();
+        boolean[][] dp = new boolean[n][n];
+
+        for (int i=0; i<n; i++) dp[i][i] = true;
+
+        int maxLen = 1;
+        int maxi = 0, maxj = 0;
+        for (int L=2; L<=n; L++) {
+            for (int i=0; i<=n-L; i++) {
+                int j = i+L-1;
+                if (s.charAt(i) == s.charAt(j)) {
+                    if (L == 2) dp[i][j] = true;
+                    else dp[i][j] = dp[i+1][j-1];
+                }
+
+                if (dp[i][j]) {
+                    maxLen = L;
+                    maxi = i;
+                    maxj = j;
+                }
+            }
+        }
+
+        return s.substring(maxi, maxj+1);
+    }
     /**
      Note how this is a variant of dp (albeit top-down)
      which doesn't come into the knap sack (pick/notPick)

8d10628362d8c9c6bf591c39e123e517ffd7ba0f
shubham88fru <shubham88fru@yahoo.com>
2026-01-31 15:01:50 -0600

    meeting rooms II

--- src/main/java/ptrn/mergeintervals/MeetingRoomsII.java
+++ src/main/java/ptrn/mergeintervals/MeetingRoomsII.java
@@ -8,15 +8,16 @@ import java.util.PriorityQueue;
 //@check - https://www.educative.io/module/page/Z4JLg2tDQPVv6QjgO/10370001/4976190424350720/6375390508744704
 //         https://www.youtube.com/watch?v=FdzJmTCVyJU&t=512s&ab_channel=NeetCode
 //@tag - TO_REVISIT
-/***
- * Directly copied the algo from edctv.
- */
 public class MeetingRoomsII {
-    public static int findSets(int[][] intervals) {
+    public int findSets(int[][] intervals) {
+//        return edctvSol(intervals);
+        return revise(intervals);
+    }
 
+    private int edctvSol(int[][] intervals) {
         /*
-        * Edctv's approach. Didn't understand need to revisit.
-        * */
+         * Edctv's approach. Didn't understand need to revisit.
+         * */
         Arrays.sort(intervals, (a1, a2) -> a1[0] - a2[0]);
         PriorityQueue<Integer> pq = new PriorityQueue<>();
         pq.add(intervals[0][1]);
@@ -40,38 +41,38 @@ public class MeetingRoomsII {
        This problems basically boils down
        to finding the maximum number of
        overlapping intervals. Coz,
-       the max number number of overlapping
+       the max number of overlapping
        intervals will be the meeting rooms required.
    */
-    private int revise(List<Interval> intervals) {
-        int n = intervals.size();
+    private int revise(int[][] intervals) {
+        int n = intervals.length;
         int[] start = new int[n];
         int[] end = new int[n];
 
         for (int i=0; i<n; i++) {
-            start[i] = intervals.get(i).start;
-            end[i] = intervals.get(i).end;
+            start[i] = intervals[i][0];
+            end[i] = intervals[i][1];
         }
 
         Arrays.sort(start);
         Arrays.sort(end);
 
-        int max = 0;
-        int p1 = 0;
-        int p2 = 0;
         int rooms = 0;
-        while (p1 < n && p2 < n) {
-            if (start[p1] < end[p2]) { //new meeting started.
-                p1 += 1;
-                rooms += 1; //new meeting started.
-                max = Math.max(max, rooms);
-            } else { //a meeting ended.
-                p2 += 1;
+        int minRooms = 0;
+        int i=0, j=0;
+
+        while (i < n && j < n) {
+            if (start[i] < end[j]) {
+                i += 1;
+                rooms += 1;
+                minRooms = Math.max(minRooms, rooms);
+            } else {
+                j += 1;
                 rooms -= 1;
             }
         }
 
-        return max;
+        return minRooms;
     }
 
 }

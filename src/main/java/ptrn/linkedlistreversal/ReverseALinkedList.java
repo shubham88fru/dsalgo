650df50e7c393823e8078995e21e6bcb0216f219
shubham88fru <shubham88fru@yahoo.com>
2026-02-01 07:46:39 -0600

    reverse a linked list

--- src/main/java/ptrn/linkedlistreversal/ReverseALinkedList.java
+++ src/main/java/ptrn/linkedlistreversal/ReverseALinkedList.java
@@ -5,73 +5,65 @@ package ptrn.linkedlistreversal;
 //@check - https://www.educative.io/module/page/Z4JLg2tDQPVv6QjgO/10370001/4976190424350720/4776723687407616
 public class ReverseALinkedList {
     public ListNode reverseList(ListNode head) {
-        return sol1(head);
-        //return sol2(head);
-        //return recursive(head);
+        // return iterative(head);
+        // return sol2(head);
+        return sol3(head);
     }
 
-    //Based on strvr's explanation.
-    private ListNode recursive(ListNode head) {
-        if (head == null || head.next == null) {
-            return head;
+    private ListNode sol3(ListNode head) {
+        return recursive(head);
+    }
+
+    private ListNode recursive(ListNode curr) {
+        if (curr == null || curr.next == null) { //og tail.
+            return curr;
         }
 
-        ListNode newHead = recursive(head.next);
-        ListNode nxt = head.next;
-        nxt.next = head;
-        head.next = null;
-        return newHead;
+        ListNode head = recursive(curr.next);
+        ListNode next = curr.next;
+        next.next = curr;
+        curr.next = null;
+
+        return head;
     }
 
-    //1) With prev, curr and next pointer.
-    //Needs extra code to rearrange last 2 nodes.
-    private ListNode sol1(ListNode head) {
-        if (head == null) return null;
-
-        //start with 3 pointers
-        ListNode prev = null;
-        ListNode curr = head;
-        ListNode next = curr.next; //next initialized with curr's next.
-
-        //while next not null..
-        while (next != null) {
-            curr.next = prev; //curr node points backward.
-            prev = curr; //prev moves to curr
-            curr = next; //curr moves to next
-            next = curr.next; //and next to curr's next.
-        }
-
-        //at this point, curr is pointing to last element
-        //and prev to second last. So, do the last move.
-        curr.next = prev;
-        prev = curr;
-
-        //prev is the new head.
-        return prev;
-    }
-
-    //2) No extra code to handle rearranging of last 2 nodes.
-    //T: O(N), S: O(1)
+    /**
+     My recursive soln.
+     Not very clean.
+     */
     private ListNode sol2(ListNode head) {
+        ListNode dummy = new ListNode(-1);
+        ListNode ogHead = recursive2(head, dummy);
+        if (ogHead != null) ogHead.next = null;
+        return dummy.next;
+    }
+
+    private ListNode recursive2(ListNode curr, ListNode dummy) {
+
+        if (curr == null || curr.next == null) {
+            dummy.next = curr;
+            return curr;
+        }
+
+
+        ListNode ogNext = recursive2(curr.next, dummy);
+        ogNext.next = curr;
+
+        return curr;
+    }
+
+    private ListNode iterative(ListNode head) {
         if (head == null) return null;
 
-        ListNode prev = null;
-        ListNode curr = head;
-        ListNode next = null; //next initialized to null.
+        ListNode prev = null, curr = head;
 
-        //while curr not null..
         while (curr != null) {
-            //store next, so we can break cur-next link.
-            next = curr.next;
-            //break curr-next link and reverse it.
+            ListNode next = curr.next;
             curr.next = prev;
-            //move prev to curr
             prev = curr;
-            //move curr to next.
             curr = next;
         }
 
-        //prev is the new head.
         return prev;
     }
 }

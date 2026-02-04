dea37207fde2e784734f0fa84ba8f9ce0ae3a002
shubham88fru <shubham88fru@yahoo.com>
2026-02-03 18:38:59 -0600

    product of array except self

--- src/main/java/ptrn/twopointers/ProductOfArrayExceptSelf.java
+++ src/main/java/ptrn/twopointers/ProductOfArrayExceptSelf.java
@@ -4,36 +4,53 @@ package ptrn.twopointers;
 //@check - https://www.educative.io/module/page/Z4JLg2tDQPVv6QjgO/10370001/4976190424350720/4779083553832960
 public class ProductOfArrayExceptSelf {
     public int[] productExceptSelf(int[] nums) {
-        return revise(nums);
+        // return revise(nums);
+        return optimal(nums);
     }
 
-    /*
-        My soln. I guess this on the optimal track,
-        but LC followup says that this could be
-        solved in O(1) extra space. So, my soln might
-        not be the best soln. Therefore, if this problem
-        is reoccurring problem for some company, then
-        check YT for an even better soln.
+    /**
+     Without using extra space.
+     Idea is to use the ans array
+     itself as the pp and then
+     do another iteration of suffix.
+     */
+    private int[] optimal(int[] nums) {
+        int n = nums.length;
+
+        int[] ans = new int[n];
+        ans[0] = 1;
+        for (int i=1; i<n; i++) {
+            ans[i] = ans[i-1]*nums[i-1];
+        }
+
+        int sp = 1;
+        for (int i=n-1; i>=0; i--) {
+            ans[i] = ans[i]*sp;
+            sp *= nums[i];
+        }
+
+        return ans;
+    }
+
+    /**
+     Using prefix and suffix arrays
      */
     private int[] revise(int[] nums) {
         int n = nums.length;
-        int[] prefixProduct = new int[n];
-        int[] suffixProduct = new int[n];
 
-        prefixProduct[0] = 1;
-        suffixProduct[n-1] = 1;
+        int[] pp = new int[n];
+        int[] sp = new int[n];
+        pp[0] = 1;
+        sp[n-1] = 1;
 
         for (int i=1; i<n; i++) {
-            prefixProduct[i] = prefixProduct[i-1] * nums[i-1];
-        }
-
-        for (int i=n-2; i>=0; i--) {
-            suffixProduct[i] = suffixProduct[i+1] * nums[i+1];
+            pp[i] = pp[i-1]*nums[i-1];
+            sp[n-1-i] = sp[n-i]*nums[n-i];
         }
 
         int[] ans = new int[n];
         for (int i=0; i<n; i++) {
-            ans[i] = (prefixProduct[i] * suffixProduct[i]);
+            ans[i] = pp[i]*sp[i];
         }
 
         return ans;

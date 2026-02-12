5a5a82ba259c1f248b7ed56959aec01b8f098483
shubham88fru <shubham88fru@yahoo.com>
2026-02-12 17:32:05 -0600

    add notes

--- src/main/java/ptrn/trees/bfs/BinaryTreeLevelOrderTraversal.java
+++ src/main/java/ptrn/trees/bfs/BinaryTreeLevelOrderTraversal.java
@@ -9,47 +9,53 @@ import java.util.List;
 //@check - https://www.educative.io/module/page/Z4JLg2tDQPVv6QjgO/10370001/4976190424350720/4874740880900096
 public class BinaryTreeLevelOrderTraversal {
     public List<List<Integer>> levelOrder(TreeNode root) {
-        List<List<Integer>> bfsTraversal = new ArrayList<>();
-
-        bfs(root, bfsTraversal);
-
-        return bfsTraversal;
+        // return recursive(root);
+        return iterative(root);
     }
 
-    private void bfs(TreeNode root, List<List<Integer>> bfsTraversal) {
+    private List<List<Integer>> recursive(TreeNode root) {
+        List<List<Integer>> ans = new ArrayList<>();
+        dfs(root, 0, ans);
+        return ans;
+    }
+
+    private void dfs(TreeNode root, int d, List<List<Integer>> ans) {
         if (root == null) return;
 
-        //Initialize the queue. We use a queue for BF Traversal.
-        Deque<TreeNode> queue = new ArrayDeque<>();
+        if (ans.size()-1 < d) ans.add(new ArrayList<>());
+        ans.get(d).add(root.val);
 
-        queue.addLast(root);
+        dfs(root.left, d+1, ans);
+        dfs(root.right, d+1, ans);
+    }
 
-        while (!queue.isEmpty()) {
-            List<Integer> level = new ArrayList<>();
+    private List<List<Integer>> iterative(TreeNode root) {
+        return bfs(root);
+    }
 
-            //At any level, queue will have only the nodes at
-            //that level.
-            int remainingNodeToProcess = queue.size();
+    private List<List<Integer>> bfs(TreeNode root) {
 
-            //Process all nodes at current level
-            while (remainingNodeToProcess != 0) {
-                //keep removing the nodes untill
-                //all nodes of that level are processed.
-                TreeNode currNode = queue.removeFirst();
-                level.add(currNode.val);
-                remainingNodeToProcess -= 1;
+        List<List<Integer>> ans = new ArrayList<>();
+        if (root == null) return ans;
 
-                //Add left child of curr node to queue.
-                if (currNode.left != null) {
-                    queue.addLast(currNode.left);
-                }
+        Deque<TreeNode> q = new ArrayDeque<>();
+        q.addLast(root);
 
-                //Add right child of curr node to queue.
-                if (currNode.right != null) {
-                    queue.addLast(currNode.right);
-                }
+        while (!q.isEmpty()) {
+            int sz = q.size();
+            List<Integer> lvl = new ArrayList<>();
+            while (sz > 0) {
+                TreeNode node = q.removeFirst();
+                lvl.add(node.val);
+
+                if (node.left != null) q.addLast(node.left);
+                if (node.right != null) q.addLast(node.right);
+
+                sz -= 1;
             }
-            bfsTraversal.add(level);
+            ans.add(lvl);
         }
+
+        return ans;
     }
 }

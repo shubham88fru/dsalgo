461b7ad53d8bf45a886e7c5a716c257ecb8d5b25
shubham88fru <shubham88fru@yahoo.com>
2026-02-13 09:27:28 -0600

    find if path exists in graph

--- src/main/java/ptrn/graph/FindIfPathExists.java
+++ src/main/java/ptrn/graph/FindIfPathExists.java
@@ -1,9 +1,6 @@
 package ptrn.graph;
 
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Deque;
-import java.util.List;
+import java.util.*;
 
 //@link - https://leetcode.com/problems/find-if-path-exists-in-graph/description/
 //@check - https://www.educative.io/module/page/Z4JLg2tDQPVv6QjgO/10370001/4976190424350720/5758360890376192
@@ -15,40 +12,36 @@ public class FindIfPathExists {
         doing the union find pattern from edctv.
     */
     public boolean validPath(int n, int[][] edges, int source, int destination) {
-        List<List<Integer>> graph = getGraph(n, edges);
-        return bfsForValidPath(graph, n, source, destination);
+        return recursion(n, edges, source, destination);
     }
 
-    private boolean bfsForValidPath(List<List<Integer>> graph, int n, int src, int dest) {
-        Deque<Integer> q = new ArrayDeque<>();
-        int[] visited = new int[n];
-        q.addLast(src);
-        visited[src] = 1;
-        while (!q.isEmpty()) {
-            int node = q.removeFirst();
-            if (node == dest) return true;
-            visited[node] = 1;
-            List<Integer> neighbours = graph.get(node);
-            for (int neighbour: neighbours) {
-                if (visited[neighbour] != 1) q.addLast(neighbour);
-            }
-        }
-
-        return false;
-
-    }
-
-    private List<List<Integer>> getGraph(int n, int[][] edges) {
+    /*
+        UPDATE: This DFS approach of mine works.
+    */
+    private boolean recursion(int n, int[][] edges, int source, int destination) {
         List<List<Integer>> graph = new ArrayList<>();
         for (int i=0; i<n; i++) graph.add(new ArrayList<>());
-        for (int[] edge: edges) {
-            int u = edge[0];
-            int v = edge[1];
 
+        for (int[] edge: edges) {
+            int u = edge[0], v = edge[1];
             graph.get(u).add(v);
             graph.get(v).add(u);
         }
 
-        return graph;
+        Set<Integer> visited = new HashSet<>();
+        return dfs(graph, source, destination, visited);
+    }
+
+    private boolean dfs(List<List<Integer>> graph, int s, int d, Set<Integer> v) {
+        if (v.contains(s)) return false;
+
+        if (s == d) return true;
+        v.add(s);
+
+        for (int ng: graph.get(s)) {
+            if(dfs(graph, ng, d, v)) return true;
+        }
+
+        return false;
     }
 }

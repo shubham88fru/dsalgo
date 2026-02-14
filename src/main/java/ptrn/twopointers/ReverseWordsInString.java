f671abf693546a24d24ed85b5024ada9fa0a99dc
shubham88fru <shubham88fru@yahoo.com>
2026-02-14 17:22:50 -0600

    reverse words in string

--- src/main/java/ptrn/twopointers/ReverseWordsInString.java
+++ src/main/java/ptrn/twopointers/ReverseWordsInString.java
@@ -1,76 +1,48 @@
 package ptrn.twopointers;
 
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 
 //@link - https://leetcode.com/problems/reverse-words-in-a-string/
 //@strvr - https://takeuforward.org/data-structure/reverse-words-in-a-string/
 //@check - https://www.educative.io/module/page/Z4JLg2tDQPVv6QjgO/10370001/4976190424350720/4737418008985600
 public class ReverseWordsInString {
     public String reverseWords(String s) {
-        //return reverse(s);
-        return reverseWordOptimal(s);
+        return optimal(s);
+        // return brute(s);
     }
 
-    //1) (kinda) optimal solution ?
-    private String reverseWordOptimal(String s) {
-        //reverse the entire string.
-        StringBuffer sb = new StringBuffer(s.trim());
-        sb.reverse();
+    private String optimal(String s) {
+        int n = s.length();
+        StringBuffer sb = new StringBuffer();
 
-        StringBuffer ans = new StringBuffer();
-
-        int start = 0;
-        int end = 0;
-        while (end < sb.length()) {
-            //for each word, reverse the word and
-            //append to the answer string. NOTE: Need
-            //to handle the spaces properly.
-            if (sb.charAt(end) == ' ') {
-                String sub = sb.substring(start, end);
-                String reversed = (new StringBuffer(sub)).reverse().toString().trim();
-                if (reversed.length() > 0) {
-                    ans.append(reversed);
-                    ans.append(" ");
-                }
-
-                //when word is done, start moves to
-                //start of the next word (i.e. after the space)
-                start = end + 1;
+        int i=n-1;
+        while (i >= 0) {
+            while (i >= 0 && !Character.isLetterOrDigit(s.charAt(i))) {
+                i -= 1;
             }
 
-            //keep moving the end untill
-            //word end is ecountered.
-            end += 1;
+            int j=i;
+            while (i >=0 && Character.isLetterOrDigit(s.charAt(i))) {
+                i -= 1;
+            }
+
+            sb.append(s.substring(i+1, j+1));
+            sb.append(" ");
+
+            i -= 1;
         }
 
-        //process the last word.
-        String sub = sb.substring(start, end);
-        String reversed = (new StringBuffer(sub)).reverse().toString().trim();
-        if (reversed.length() > 0) {
-            ans.append(reversed);
-        }
-
-        return ans.toString();
+        return sb.toString().trim();
     }
 
-    //2) Kinda naive solution
-    private String reverse(String s) {
-        String[] arr = Arrays.stream(s.trim().split(" "))
-                .filter(str -> !str.equals(""))
-                .toArray(String[]::new);
+    private String brute(String str) {
+        List<String> rev = Arrays.stream(str.split(" "))
+                .map(s -> s.trim()).filter(s -> s.length()>0).collect(Collectors.toList());
+        Collections.reverse(rev);
 
-        int start = 0;
-        int end = arr.length-1;
-
-        while (start <= end) {
-            String temp = arr[start].trim();
-            arr[start] = arr[end].trim();
-            arr[end] = temp;
-
-            start += 1;
-            end -= 1;
-        }
-
-        return String.join(" ", arr);
+        return rev.stream().collect(Collectors.joining(" "));
     }
 }

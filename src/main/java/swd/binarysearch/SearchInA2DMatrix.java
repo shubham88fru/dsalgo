f4d9ad7084a535199b5340906d2e3c234514b08e
shubham88fru <shubham88fru@yahoo.com>
2026-02-21 19:02:53 -0600

    add notes

--- src/main/java/swd/binarysearch/SearchInA2DMatrix.java
+++ src/main/java/swd/binarysearch/SearchInA2DMatrix.java
@@ -30,6 +30,26 @@ public class SearchInA2DMatrix {
         return false;
     }
 
+    //works with the code for part II of this
+    //problem as well
+    private boolean suboptimal(int[][] matrix, int target) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        int i=0, j=n-1;
+        while (i >= 0 && i < m && j >= 0 && j < n) {
+            if (matrix[i][j] == target) return true;
+
+            if (target < matrix[i][j]) {
+                j -= 1;
+            } else {
+                i += 1;
+            }
+        }
+
+        return false;
+    }
+
     //2) O(row*log(col))
     private boolean binarySearch2(int[][] matrix, int target) {
         int m = matrix.length;

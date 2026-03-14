888eb48a8f0fa6918feab8cb2e554a38102ef429
shubham88fru <shubham88fru@yahoo.com>
2026-03-14 14:11:38 -0500

    path with maximum gold

--- src/main/java/lc_potd/PathWithMaximumGold.java
+++ src/main/java/lc_potd/PathWithMaximumGold.java
@@ -46,4 +46,32 @@ public class PathWithMaximumGold {
         max[0] = Math.max(gold, max[0]);
         visited[i][j] = 0;
     }
+
+    /// ///////////////////////////////////////////
+    private int revise(int[][] grid) {
+        int m = grid.length, n = grid[0].length;
+
+        int max = 0;
+        for (int i=0; i<m; i++) {
+            for (int j=0; j<n; j++) {
+                if (grid[i][j] != 0) {
+                    max = Math.max(max, dfs(grid, m, n, i, j));
+                }
+            }
+        }
+        return max;
+    }
+
+    private int dfs(int[][] grid, int m, int n, int i, int j) {
+        if (i < 0 || i >= m || j < 0 || j >= n || grid[i][j] == -1 || grid[i][j] == 0) return 0;
+        int coins = grid[i][j];
+        grid[i][j] = -1;
+        int up = coins + dfs(grid, m, n, i-1, j);
+        int right = coins + dfs(grid, m, n, i, j+1);
+        int down = coins + dfs(grid, m, n, i+1, j);
+        int left = coins + dfs(grid, m, n, i, j-1);
+        grid[i][j] = coins;
+
+        return Math.max(up, Math.max(right, Math.max(down, left)));
+    }
 }

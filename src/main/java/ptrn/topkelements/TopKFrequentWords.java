b4c474dae435683ea0c374db11ce218cd140cec4
shubham88fru <shubham88fru@yahoo.com>
2026-03-15 08:51:54 -0500

    top k frequent words

--- src/main/java/ptrn/topkelements/TopKFrequentWords.java
+++ src/main/java/ptrn/topkelements/TopKFrequentWords.java
@@ -6,6 +6,36 @@ import java.util.*;
 //@check - https://www.educative.io/module/page/Z4JLg2tDQPVv6QjgO/10370001/4976190424350720/6184234815062016
 public class TopKFrequentWords {
     public List<String> topKFrequent(String[] words, int k) {
+//        return edctvSol(words, k);
+        return revise(words, k);
+    }
+
+    private List<String> revise(String[] words, int k) {
+        int n = words.length;
+        Map<String, Integer> freq = new HashMap<>();
+        for (String word: words) freq.put(word, freq.getOrDefault(word, 0)+1);
+
+        Comparator<WordWrapper> c1 = (p1, p2) -> p1.freq - p2.freq;
+        Comparator<WordWrapper> c2 = (p1, p2) -> p2.word.compareTo(p1.word); //!!IMPORTANT
+        Comparator<WordWrapper> c3 = c1.thenComparing(c2);
+        PriorityQueue<WordWrapper> pq = new PriorityQueue<>(c3);
+
+        for (Map.Entry<String, Integer> entry: freq.entrySet()) {
+            WordWrapper p = new WordWrapper(entry.getKey(), entry.getValue());
+            pq.add(p);
+            if (pq.size() > k) pq.remove();
+        }
+
+        List<String> ans = new ArrayList<>(k);
+        while (!pq.isEmpty()) {
+            ans.add(pq.remove().word);
+        }
+
+        Collections.reverse(ans);
+        return ans;
+    }
+
+    private List<String> edctvSol(String[] words, int k) {
         Comparator<WordWrapper> cmp = (ww1, ww2) -> ww1.freq - ww2.freq;
         Comparator<WordWrapper> finalCmp = cmp.thenComparing((ww1, ww2) -> ww2.word.compareTo(ww1.word));
         PriorityQueue<WordWrapper> minHeap =

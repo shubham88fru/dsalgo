7fa16c2ec5bf2c90ef8a8033a6d34f2beda1e9fd
shubham88fru <shubham88fru@yahoo.com>
2026-05-06 08:25:45 -0500

    rotating the box

--- src/main/java/lc_potd/RotatingTheBox.java
+++ src/main/java/lc_potd/RotatingTheBox.java
@@ -42,6 +42,59 @@ public class RotatingTheBox {
         return tr;
     }
 
+    /// /////////////////////////////////////////////
+    private char[][] suboptimal(char[][] boxGrid) {
+        int m = boxGrid.length;
+        int n = boxGrid[0].length;
+
+        for (int i=0; i<m; i++) {
+            for (int j=n-1; j>=0; j--) {
+                if (boxGrid[i][j] == '#') {
+                    int jj = j+1;
+                    int pos = -1;
+                    while (jj < n && boxGrid[i][jj] != '*' && boxGrid[i][jj] == '.') {
+                        pos = jj;
+                        jj += 1;
+                    }
+
+                    if (pos != -1) {
+                        boxGrid[i][pos] = '#';
+                        boxGrid[i][j] = '.';
+                    }
+                }
+            }
+        }
+
+        char[][] mat = rotate(boxGrid);
+        return mat;
+    }
+
+    private char[][] rotate(char[][] boxGrid) {
+        int m = boxGrid.length;
+        int n = boxGrid[0].length;
+
+        char[][] rotated = new char[n][m];
+        for (int i=0; i<m; i++) {
+            for (int j=0; j<n; j++) {
+                rotated[j][i] = boxGrid[i][j];
+            }
+        }
+
+        for (int i=0; i<n; i++) {
+            int l=0, r=m-1;
+            while (l <= r) {
+                char temp = rotated[i][l];
+                rotated[i][l] = rotated[i][r];
+                rotated[i][r] = temp;
+                l += 1;
+                r -= 1;
+            }
+        }
+
+        return rotated;
+    }
+    /// /////////////////////////////////////////////
+
     private void printMatrix(char[][] box) {
         int m = box.length;
         int n = box[0].length;

13e38d52f18a03338d466437f14b998d6d4308ae
shubham88fru <shubham88fru@yahoo.com>
2026-07-18 07:06:17 -0700

    refactor

--- src/main/java/lc_potd/SumOfGCDOfFormedPairs.java
+++ src/main/java/lc_potd/SumOfGCDOfFormedPairs.java
@@ -30,7 +30,7 @@ public class SumOfGCDOfFormedPairs {
         return sum;
     }
 
-    public int gcd(int a, int b) {
+    private int gcd(int a, int b) {
         if (b == 0) return a;
         return gcd(b, a % b);
     }
